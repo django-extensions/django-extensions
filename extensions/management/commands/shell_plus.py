a2e465954589e5dc3417a25c8907bddc6cce99cb
Jannis Leidel <jannis@leidel.info>
2008-01-10 16:30:43 +0000

    fixed the shell_plus command which now actually loads the models of all installed apps

--- extensions/management/commands/shell_plus.py
+++ extensions/management/commands/shell_plus.py
@@ -1,28 +1,65 @@
+import os
 from django.core.management.base import NoArgsCommand
 from optparse import make_option
 
 class Command(NoArgsCommand):
     option_list = NoArgsCommand.option_list + (
-        make_option('--verbosity', action='store', dest='verbosity', default='1',
-            type='choice', choices=['0', '1', '2'],
-            help='Verbosity level; 0=minimal output, 1=normal output, 2=all output'),
         make_option('--plain', action='store_true', dest='plain',
             help='Tells Django to use plain Python, not IPython.'),
     )
     help = "Like the 'shell' command but autoloads the models of all installed Django apps."
-    
+
     requires_model_validation = True
-    
+
     def handle_noargs(self, **options):
-        from django.core.management import call_command
-        from django.db.models.loading import get_models
-        
-        verbosity = int(options.get('verbosity', 1))
+        # XXX: (Temporary) workaround for ticket #1796: force early loading of all
+        # models from installed apps.
+        from django.db.models.loading import get_models, get_apps
+        loaded_models = get_models()
+
         use_plain = options.get('plain', False)
-        
-        for m in get_models():
-            try:
-                exec "from %s import %s" % (m.__module__, m.__name__)
+
+        try:
+            if use_plain:
+                # Don't bother loading IPython, because the user wants plain Python.
+                raise ImportError
+            import IPython
+            # Explicitly pass an empty list as arguments, because otherwise IPython
+            # would use sys.argv from this script.
+            shell = IPython.Shell.IPShell(argv=[])
+            shell.mainloop()
+        except ImportError:
+            import code
+            # Set up a dictionary to serve as the environment for the shell, so
+            # that tab completion works on objects that are imported at runtime.
+            # See ticket 5082.
+            imported_objects = {}
+            try: # Try activating rlcompleter, because it's handy.
+                import readline
             except ImportError:
                 pass
-        call_command('shell', **{'use_plain': use_plain})
+            else:
+                # We don't have to wrap the following import in a 'try', because
+                # we already know 'readline' was imported successfully.
+                import rlcompleter
+                readline.set_completer(rlcompleter.Completer(imported_objects).complete)
+                readline.parse_and_bind("tab:complete")
+
+            # We want to honor both $PYTHONSTARTUP and .pythonrc.py, so follow system
+            # conventions and get $PYTHONSTARTUP first then import user.
+            if not use_plain: 
+                pythonrc = os.environ.get("PYTHONSTARTUP") 
+                if pythonrc and os.path.isfile(pythonrc): 
+                    try: 
+                        execfile(pythonrc) 
+                    except NameError: 
+                        pass
+                # This will import .pythonrc.py as a side-effect
+                import user
+                for app_mod in get_apps():
+                    app_models = get_models(app_mod)
+                    model_labels = ", ".join([model.__name__ for model in app_models])
+                    print "From '%s' autoloaded: %s" % (app_mod.__name__.split('.')[-2], model_labels)
+                    for model in app_models:
+                        imported_objects[model.__name__] = getattr(__import__(app_mod.__name__, {}, {}, model.__name__), model.__name__)
+            code.interact(local=imported_objects)
