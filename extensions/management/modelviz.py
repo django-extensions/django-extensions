2d86f7be52c474522a135ef8ad4a0cd7f644c40d
Bas van Oostveen <v.oostveen@gmail.com>
2008-05-10 21:48:54 +0000

    Applied patch from jfindlay, thanks !! Fixed problem with getopts and --all-applications flag Corrected indentation, original file had weird indentations for multiple line list and dictionary declarations

--- extensions/management/modelviz.py
+++ extensions/management/modelviz.py
@@ -6,17 +6,26 @@ Make sure your DJANGO_SETTINGS_MODULE is set to your project or
 place this script in the same directory of the project and call
 the script like this:
 
-$ python modelviz.py [-h] [-d] <app_label> ... <app_label> > <filename>.dot
+$ python modelviz.py [-h] [-a] [-d] [-g] [-i <model_names>] <app_label> ... <app_label> > <filename>.dot
 $ dot <filename>.dot -Tpng -o <filename>.png
 
 options:
     -h, --help
     show this help message and exit.
 
+    -a, --all_applications
+    show models from all applications.
+
     -d, --disable_fields
     don't show the class member fields.
+
+    -g, --group_models
+    draw an enclosing box around models from the same app.
+
+    -i, --include_models=User,Person,Car
+    only include selected models in graph.
 """
-__version__ = "0.8"
+__version__ = "0.9"
 __svnid__ = "$Id$"
 __license__ = "Python"
 __author__ = "Antonio Cavedoni <http://cavedoni.com/>"
@@ -26,7 +35,9 @@ __contributors__ = [
    "Carlo C8E Miron",
    "Andre Campos <cahenan@gmail.com>",
    "Justin Findlay <jfindlay@gmail.com>",
-   ]
+   "Alexander Houben <alexander@houben.ch>",
+   "Bas van Oostveen <v.oostveen@gmail.com>",
+]
 
 import getopt, sys
 
@@ -131,15 +142,16 @@ tail_template = """
 
 def generate_dot(app_labels, **kwargs):
     disable_fields = kwargs.get('disable_fields', False)
+    include_models = kwargs.get('include_models', [])
     all_applications = kwargs.get('all_applications', False)
     use_subgraph = kwargs.get('group_models', False)
-    
+
     dot = head_template
 
     apps = []
     if all_applications:
         apps = models.get_apps()
-    
+
     for app_label in app_labels:
         app = models.get_app(app_label)
         if not app in apps:
@@ -154,14 +166,21 @@ def generate_dot(app_labels, **kwargs):
             'disable_fields': disable_fields,
             'use_subgraph': use_subgraph,
             'models': []
-            })
+        })
 
         for appmodel in get_models(app):
             model = {
                 'name': appmodel.__name__,
                 'fields': [],
                 'relations': []
-                }
+            }
+
+            # consider given model name ?
+            def consider(model_name):
+                return not include_models or model_name in include_models
+
+            if not consider(appmodel._meta.object_name):
+                continue
 
             # model attributes
             def add_attributes():
@@ -169,7 +188,7 @@ def generate_dot(app_labels, **kwargs):
                     'name': field.name,
                     'type': type(field).__name__,
                     'blank': field.blank
-                    })
+                })
 
             for field in appmodel._meta.fields:
                 add_attributes()
@@ -186,23 +205,22 @@ def generate_dot(app_labels, **kwargs):
                     'name': field.name,
                     'arrows': extras,
                     'needs_node': True
-                    }
-                if _rel not in model['relations']:
+                }
+                if _rel not in model['relations'] and consider(_rel['target']):
                     model['relations'].append(_rel)
 
             for field in appmodel._meta.fields:
                 if isinstance(field, ForeignKey):
                     add_relation()
                 elif isinstance(field, OneToOneField):
-                    add_relation("[arrowhead=none arrowtail=none]")
+                    add_relation('[arrowhead=none arrowtail=none]')
 
             if appmodel._meta.many_to_many:
                 for field in appmodel._meta.many_to_many:
                     if isinstance(field, ManyToManyField):
-                        add_relation("[arrowhead=normal arrowtail=normal]")
+                        add_relation('[arrowhead=normal arrowtail=normal]')
                     elif isinstance(field, GenericRelation):
-                        add_relation(
-                            '[style="dotted"] [arrowhead=normal arrowtail=normal]')
+                        add_relation('[style="dotted"] [arrowhead=normal arrowtail=normal]')
             graph['models'].append(model)
         graphs.append(graph)
 
@@ -230,23 +248,30 @@ def generate_dot(app_labels, **kwargs):
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hd",
-                    ["help", "disable_fields"])
+        opts, args = getopt.getopt(sys.argv[1:], "hadgi:",
+                    ["help", "all_applications", "disable_fields", "group_models", "include_models="])
     except getopt.GetoptError, error:
         print __doc__
         sys.exit(error)
-    else:
-        if not args:
-            print __doc__
-            sys.exit()
-
+    
     kwargs = {}
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             print __doc__
             sys.exit()
+        if opt in ("-a", "--all_applications"):
+            kwargs['all_applications'] = True
         if opt in ("-d", "--disable_fields"):
             kwargs['disable_fields'] = True
+        if opt in ("-g", "--group_models"):
+            kwargs['group_models'] = True
+        if opt in ("-i", "--include_models"):
+            kwargs['include_models'] = arg.split(',')
+
+    if not args and not kwargs.get('all_applications', False):
+        print __doc__
+        sys.exit()
+
     print generate_dot(args, **kwargs)
 
 if __name__ == "__main__":
