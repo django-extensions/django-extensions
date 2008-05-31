d89d5e5e6d678961806ad0c6d641f2884bcab015
Bas van Oostveen <v.oostveen@gmail.com>
2008-05-31 16:30:04 +0000

    allow settings the default password from the command line (making the command scriptable)

--- extensions/management/commands/set_fake_passwords.py
+++ extensions/management/commands/set_fake_passwords.py
@@ -1,11 +1,11 @@
-'''
+"""
 set_fake_passwords.py 
 
     Reset all user passwords to a common value. Useful for testing in a 
     development environment. As such, this command is only available when
     setting.DEBUG is True.
 
-'''
+"""
 from optparse import make_option
 
 from django.conf import settings
@@ -17,6 +17,8 @@ class Command(NoArgsCommand):
     option_list = NoArgsCommand.option_list + (
         make_option('--prompt', dest='prompt_passwd', default=False, action='store_true',
             help='Prompts for the new password to apply to all users'),
+        make_option('--password', dest='default_passwd', default=DEFAULT_FAKE_PASSWORD,
+            help='Use this as default password.'),
     )
     help = 'DEBUG only: sets all user passwords to a common value ("%s" by default)' % (DEFAULT_FAKE_PASSWORD, )
     requires_model_validation = False
@@ -32,7 +34,7 @@ class Command(NoArgsCommand):
             if not passwd:
                 raise CommandError('You must enter a valid password')
         else:
-            passwd = DEFAULT_FAKE_PASSWORD
+            passwd = options.get('default_passwd', DEFAULT_FAKE_PASSWORD)
         
         users = User.objects.all()
         for user in users:
