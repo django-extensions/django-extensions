e8937d825ec7f13f8b3d3f93314bdbe949bbb017
Bas van Oostveen <v.oostveen@gmail.com>
2008-07-17 14:14:12 +0000

    fix for issue #29 daily_cleanup should for again for both pre and post change 7844 versions of django. (added reminder that this can be removed after Django hits 1.0)

--- extensions/jobs/daily/daily_cleanup.py
+++ extensions/jobs/daily/daily_cleanup.py
@@ -11,5 +11,12 @@ class Job(DailyJob):
     help = "Django Daily Cleanup Job"
 
     def execute(self):
-	from django.bin.daily_cleanup import clean_up
-	clean_up()
+	# TODO: Remove the old way when Django 1.0 lands
+	try:
+	    # old way of doing cleanup (pre r7844 in svn)
+	    from django.bin.daily_cleanup import clean_up
+	    clean_up()
+	except:
+	    # new way using the management.call_command function
+	    from django.core import management
+	    management.call_command("cleanup")
