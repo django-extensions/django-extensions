a5666a3519e2f0d42dc3065595b62efdbfe1a597
Bas van Oostveen <v.oostveen@gmail.com>
2008-08-09 15:57:52 +0000

    added UUIDField

--- django_extensions/db/fields/__init__.py
+++ django_extensions/db/fields/__init__.py
@@ -2,8 +2,13 @@
 Django Extensions additional model fields
 """
 
-from django.db.models import DateTimeField
+from django.db.models import DateTimeField, CharField
 import datetime
+
+try:
+    import uuid
+except ImportError:
+    from django_extensions.utils import uuid
 
 class CreationDateTimeField(DateTimeField):
     """ CreationDateTimeField """
@@ -42,3 +47,53 @@ class ModificationDateTimeField(DateTimeField):
     
     def get_internal_type(self):
 	return "DateTimeField"
+
+class UUIDVersionError(Exception):
+    pass
+
+class UUIDField(CharField):
+    """ UUIDField for Django, supports all uuid versions which are natively
+        suported by the uuid python module.
+    """
+
+    def __init__(self, verbose_name=None, name=None, auto=True, version=1, node=None, clock_seq=None, namespace=None, **kwargs):
+        kwargs['maxlength'] = 36
+        if auto:
+            kwargs['blank'] = True
+            kwargs['editable'] = kwargs.get('editable', False)
+        self.version = version
+        if version==1:
+            self.node, self.clock_seq = node, clock_seq
+        elif version==3 or version==5:
+            self.namespace, self.name = namespace, name
+        CharField.__init__(self, verbose_name, name, **kwargs)
+
+    def get_internal_type(self):
+        return CharField.__name__
+
+    def create_uuid(self):
+        if not self.version or self.version==4:
+            return uuid.uuid4()
+        elif self.version==1:
+            return uuid.uuid1(self.node, self.clock_seq)
+        elif self.version==2:
+            raise UUIDVersionError("UUID version 2 is not supported.")
+        elif self.version==3:
+            return uuid.uuid3(self.namespace, self.name)
+        elif self.version==5:
+            return uuid.uuid5(self.namespace, self.name)
+        else:
+            raise UUIDVersionError("UUID version %s is not valid." % self.version)
+
+    def pre_save(self, model_instance, add):
+        if self.auto and add:
+            value = unicode(self.create_uuid())
+            setattr(model_instance, self.attname, value)
+            return value
+        else:
+            value = super(UUIDField, self).pre_save(model_instance, add)
+            if self.auto and not value:
+                value = unicode(self.create_uuid())
+                setattr(model_instance, self.attname, value)
+        return value
+
