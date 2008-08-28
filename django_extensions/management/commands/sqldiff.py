2c5dd3a1aafc9a4d899cf058a8dfad0daebfeee2
Bas van Oostveen <v.oostveen@gmail.com>
2008-08-28 15:23:10 +0000

    added sql patch from SamoriGorse fixed issue #44 side note, i'm still hoping to have the time to rewrite sqldiff before 1.0. the command itself will not change but the code could (and should) be much cleaner and seperates out the logic for finding differences and printing them. (in text/sql or other form)

--- django_extensions/management/commands/sqldiff.py
+++ django_extensions/management/commands/sqldiff.py
@@ -28,7 +28,9 @@ class Command(AppCommand):
         make_option('--not-only-existing', '-e', action='store_false', dest='only_existing',
                     help="Check all tables that exist in the database, not only tables that should exist based on models."),
         make_option('--dense-output', '-d', action='store_true', dest='dense_output',
-                    help="Shows the output in dense format, normally output is spreaded over multiple lines.")
+                    help="Shows the output in dense format, normally output is spreaded over multiple lines."),
+        make_option('--output_sql', '-s', action='store_true', dest='sql',
+                    help="Outputs the differences as SQL when available"),
     )
     
     help = "Prints the (approximated) difference between models and SQL fields in the database for the given app name(s)."
@@ -45,7 +47,7 @@ class Command(AppCommand):
     def handle_app(self, app, **options):
         from django.conf import settings
         self.is_pgsql = settings.DATABASE_ENGINE.startswith("postgresql")
-        
+        self.is_sqlite = settings.DATABASE_ENGINE.startswith("sqlite")
         self.handle_diff(app, **options)
     
     @transaction.commit_manually
@@ -164,37 +166,83 @@ class Command(AppCommand):
                     c_db_field_type = clean(db_field_type)
                     c_model_type = clean(model_type)
                     if not cmp_or_serialcmp(c_model_type, c_db_field_type):
-                        diffs.append("field '%s' not of same type: db=%s, model=%s" % (att_name, c_db_field_type, c_model_type))
+                        diffs.append({
+                            'text' : "field '%s' not of same type: db=%s, model=%s" % (att_name, c_db_field_type, c_model_type),
+                            'type' : 'type',
+                            'data' : (table_name, att_name, c_db_field_type, c_model_type)
+                        })
                         continue
                     if not cmp_or_serialcmp(db_field_type, model_type):
-                        diffs.append("field '%s' parameters differ: db=%s, model=%s" % (att_name, db_field_type, model_type))
+                        diffs.append({
+                            'text' : "field '%s' parameters differ: db=%s, model=%s" % (att_name, db_field_type, model_type),
+                            'type' : 'param',
+                            'data' : (table_name, att_name, db_field_type, model_type)
+                        })
                         continue
                 else:
-                    diffs.append("field '%s' missing in model field" % att_name)
+                    diffs.append({
+                        'text' : "field '%s' missing in model field" % att_name,
+                        'type' : 'missing-in-model',
+                        'data' : (table_name, att_name, db_field_type, model_type)
+                    })
             for field in _meta.fields:
                 if field.db_index:
                     if not field.attname in table_indexes and not field.unique:
-                        diffs.append("field '%s' INDEX defined in model missing in database" % (field.attname))
+                        diffs.append({
+                            'text' : "field '%s' INDEX defined in model missing in database" % (field.attname),
+                        })
             if fieldmap:
                 for att_name, field in fieldmap.items():
-                    diffs.append("field '%s' missing in database" % att_name)
+                    diffs.append({
+                        'text' : "field '%s' missing in database" % att_name,
+                        'type' : 'missing-in-db',
+                        'data' : (table_name, att_name, field.db_type())
+                    })
             if diffs:
                 model_diffs.append((app_model.__name__, diffs))
         if model_diffs:
             NOTICE = self.style.NOTICE
             ERROR_OUTPUT = self.style.ERROR_OUTPUT
             SQL_TABLE = self.style.SQL_TABLE
-            dense = options.get('dense_output', False)
-            if not dense:
-                print ""
-                print NOTICE("+ Application:"), SQL_TABLE(app_name)
-            for model_name, diffs in model_diffs:
-                if not diffs: continue
+            SQL_FIELD = self.style.SQL_FIELD
+            SQL_COLTYPE = self.style.SQL_COLTYPE
+            SQL_KEYWORD = self.style.SQL_KEYWORD
+            modify_command = self.is_pgsql and "TYPE" or "MODIFY"
+            
+            if options.get('sql', False):
+                lines = ["", SQL_KEYWORD("BEGIN;")]
+                
+                for model_name, diffs in model_diffs:
+                    for diff in diffs:
+                        if not diff: continue
+                        if not diff.get('data', False): continue
+                        
+                        if self.is_sqlite and diff['type'] == 'param':
+                                lines.append(NOTICE('-- %s' % diff['text']))
+                                lines.append(NOTICE('-- SQLite does not feature type alteration on columns'))
+                                continue
+                        lines.append('%s %s' % (SQL_KEYWORD('ALTER TABLE'), SQL_TABLE(diff['data'][0])) )
+                        if diff['type'] == 'missing-in-db':
+                            lines.append('\t%s %s %s;' %  (SQL_KEYWORD('ADD'), SQL_FIELD(diff['data'][1]), SQL_COLTYPE(diff['data'][2]),) )
+                        if diff['type'] == 'missing-in-model':
+                            lines.append('\t%s %s;' % (SQL_KEYWORD('DROP COLUMN') , SQL_FIELD(diff['data'][1]) ))
+                        if diff['type'] in ['type', 'param']:
+                            lines.append('\t%s %s %s %s;' % (SQL_KEYWORD('ALTER'), SQL_FIELD(diff['data'][1]), SQL_KEYWORD(modify_command), SQL_COLTYPE(diff['data'][3])))                  
+                lines.append(SQL_KEYWORD("COMMIT;"))
+                
+                print "\n".join(lines)
+            else:
+                dense = options.get('dense_output', False)
                 if not dense:
-                    print NOTICE("|-+ Differences for model:"), SQL_TABLE(model_name)
-                for diff in diffs:
+                    print ""
+                    print NOTICE("+ Application:"), SQL_TABLE(app_name)
+                for model_name, diffs in model_diffs:
+                    if not diffs: continue
                     if not dense:
-                        print NOTICE("|--+"), ERROR_OUTPUT(diff)
-                    else:
-                        print NOTICE("App"), SQL_TABLE(app_name), NOTICE('Model'), SQL_TABLE(model_name), ERROR_OUTPUT(diff)
+                        print NOTICE("|-+ Differences for model:"), SQL_TABLE(model_name)
+                    for diff in diffs:
+                        if not dense:
+                            print NOTICE("|--+"), ERROR_OUTPUT(diff['text'])
+                        else:
+                            print NOTICE("App"), SQL_TABLE(app_name), NOTICE('Model'), SQL_TABLE(model_name), ERROR_OUTPUT(diff['text'])
 
