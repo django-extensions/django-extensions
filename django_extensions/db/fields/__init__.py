6a64f42f915ef89b640ac1965f6e1ff5d52dce49
Jannis Leidel <jannis@leidel.info>
2008-08-31 15:25:47 +0000

    Added AutoSlugField to django_extensions.db.fields

--- django_extensions/db/fields/__init__.py
+++ django_extensions/db/fields/__init__.py
@@ -2,13 +2,105 @@
 Django Extensions additional model fields
 """
 
-from django.db.models import DateTimeField, CharField
+from django.template.defaultfilters import slugify
+from django.db.models import DateTimeField, CharField, SlugField
 import datetime
+import re
 
 try:
     import uuid
 except ImportError:
     from django_extensions.utils import uuid
+
+class AutoSlugField(SlugField):
+    """ AutoSlugField 
+
+    By default, sets editable=False, blank=True.
+
+    Use the 'populate_from' argument to specify which field the slug is
+    populated from. Optionally use the 'separator' argument to define the used
+    separator, defaults to '-'.
+
+    Inspired by SmileyChris' Unique Slugify snippet at:
+    http://www.djangosnippets.org/snippets/690/
+    """
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('blank', True)
+        kwargs.setdefault('editable', False)
+
+        populate_from = kwargs.pop('populate_from', None)
+        if populate_from is None:
+            raise ValueError("missing 'populate_from' argument")
+        else:
+            self._populate_from = populate_from
+        self.separator = kwargs.pop('separator',  u'-')
+        super(AutoSlugField, self).__init__(*args, **kwargs)
+
+    def _slug_strip(self, value):
+        """
+        Cleans up a slug by removing slug separator characters that occur at
+        the beginning or end of a slug.
+
+        If an alternate separator is used, it will also replace any instances
+        of the default '-' separator with the new separator.
+        """
+        re_sep = '(?:-|%s)' % re.escape(self.separator)
+        value = re.sub('%s+' % re_sep, self.separator, value)
+        return re.sub(r'^%s+|%s+$' % (re_sep, re_sep), '', value)
+
+    def create_slug(self, model_instance, add):
+        # get fields to populate from and slug field to set
+        populate_field = model_instance._meta.get_field(self._populate_from)
+        slug_field = model_instance._meta.get_field(self.attname)
+        if add:
+            # slugify the original field content and set next step to 2
+            slug = slugify(getattr(model_instance, populate_field.attname))
+            next = 2
+        else:
+            # get slug from the current model instance and calculate next
+            # step from its number, clean-up
+            slug = self._slug_strip(getattr(model_instance, self.attname))
+            next = slug.split(self.separator)[-1]
+            if next.isdigit():
+                slug = self.separator.join(slug.split(self.separator)[:-1])
+                next = int(next)
+            else:
+                next = 2
+
+        # strip slug depending on max_length attribute of the slug field
+        # and clean-up
+        slug_len = slug_field.max_length
+        if slug_len:
+            slug = slug[:slug_len]
+        slug = self._slug_strip(slug)
+        original_slug = slug
+
+        # exclude the current model instance from the queryset used in finding
+        # the next valid slug
+        queryset = model_instance.__class__._default_manager.all()
+        if model_instance.pk:
+            queryset = queryset.exclude(pk=model_instance.pk)
+
+        # increases the number while searching for the next valid slug
+        # depending on the given slug, clean-up
+        while not slug or queryset.filter(**{self.attname: slug}):
+            slug = original_slug
+            end = '%s%s' % (self.separator, next)
+            end_len = len(end)
+            if slug_len and len(slug)+end_len > slug_len:
+                slug = slug[:slug_len-end_len]
+                slug = self._slug_strip(slug)
+            slug = '%s%s' % (slug, end)
+            next += 1
+        return slug
+
+    def pre_save(self, model_instance, add):
+        value = unicode(self.create_slug(model_instance, add))
+        setattr(model_instance, self.attname, value)
+        return value
+
+    def get_internal_type(self):
+        return "SlugField"
 
 class CreationDateTimeField(DateTimeField):
     """ CreationDateTimeField 
