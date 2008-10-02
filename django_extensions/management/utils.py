0216670b6f5ae65523150edb2495c0afcafaec36
Bas van Oostveen <v.oostveen@gmail.com>
2008-10-02 12:32:19 +0000

    removed frame-hacking and used settings.SETTINGS_MODULE to get the project root. this also removed a big 'hack' where 'settings.py' was used to verify (assert) that the directory on the first stackframe is actually a django project root. fixed ticket #55

--- django_extensions/management/utils.py
+++ django_extensions/management/utils.py
@@ -1,13 +1,7 @@
-from os.path import join as _j
-from os.path import dirname, abspath, isfile
-import inspect
+from django.conf import settings
 import os
 
 def get_project_root():
     """ get the project root directory """
-    f = inspect.currentframe()
-    o = inspect.getouterframes(f)
-    fn = o[-1][0].f_code.co_filename
-    root = dirname(abspath(fn))
-    assert isfile(_j(root, "settings.py")), "Could not find project root directory"
-    return root
+    settings_mod = __import__(settings.SETTINGS_MODULE, {}, {}, [''])
+    return os.path.dirname(os.path.abspath(settings_mod.__file__))
