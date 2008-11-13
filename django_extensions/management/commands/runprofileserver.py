03a7209aa02327ead3f2313a64c06c9a28d7ac7f
Bas van Oostveen <v.oostveen@gmail.com>
2008-11-13 11:15:50 +0000

    added support for kcachegrind / lsprof style output

--- django_extensions/management/commands/runprofileserver.py
+++ django_extensions/management/commands/runprofileserver.py
@@ -1,7 +1,93 @@
+"""
+runprofileserver.py
+
+    Starts a lightweight Web server with profiling enabled.
+
+Credits for kcachegrind support taken from lsprofcalltree.py go to:
+ David Allouche
+ Jp Calderone & Itamar Shtull-Trauring
+ Johan Dahlin
+"""
+
 from django.core.management.base import BaseCommand, CommandError
 from optparse import make_option
 import os
 import sys
+
+def label(code):
+    if isinstance(code, str):
+        return ('~', 0, code)    # built-in functions ('~' sorts at the end)
+    else:
+        return '%s %s:%d' % (code.co_name,
+                             code.co_filename,
+                             code.co_firstlineno)
+
+class KCacheGrind(object):
+    def __init__(self, profiler):
+        self.data = profiler.getstats()
+        self.out_file = None
+
+    def output(self, out_file):
+        self.out_file = out_file
+        print >> out_file, 'events: Ticks'
+        self._print_summary()
+        for entry in self.data:
+            self._entry(entry)
+
+    def _print_summary(self):
+        max_cost = 0
+        for entry in self.data:
+            totaltime = int(entry.totaltime * 1000)
+            max_cost = max(max_cost, totaltime)
+        print >> self.out_file, 'summary: %d' % (max_cost,)
+
+    def _entry(self, entry):
+        out_file = self.out_file
+
+        code = entry.code
+        #print >> out_file, 'ob=%s' % (code.co_filename,)
+        if isinstance(code, str):
+            print >> out_file, 'fi=~'
+        else:
+            print >> out_file, 'fi=%s' % (code.co_filename,)
+        print >> out_file, 'fn=%s' % (label(code),)
+
+        inlinetime = int(entry.inlinetime * 1000)
+        if isinstance(code, str):
+            print >> out_file, '0 ', inlinetime
+        else:
+            print >> out_file, '%d %d' % (code.co_firstlineno, inlinetime)
+
+        # recursive calls are counted in entry.calls
+        if entry.calls:
+            calls = entry.calls
+        else:
+            calls = []
+
+        if isinstance(code, str):
+            lineno = 0
+        else:
+            lineno = code.co_firstlineno
+
+        for subentry in calls:
+            self._subentry(lineno, subentry)
+        print >> out_file
+
+    def _subentry(self, lineno, subentry):
+        out_file = self.out_file
+        code = subentry.code
+        #print >> out_file, 'cob=%s' % (code.co_filename,)
+        print >> out_file, 'cfn=%s' % (label(code),)
+        if isinstance(code, str):
+            print >> out_file, 'cfi=~'
+            print >> out_file, 'calls=%d 0' % (subentry.callcount,)
+        else:
+            print >> out_file, 'cfi=%s' % (code.co_filename,)
+            print >> out_file, 'calls=%d %d' % (
+                subentry.callcount, code.co_firstlineno)
+
+        totaltime = int(subentry.totaltime * 1000)
+        print >> out_file, '%d %d' % (lineno, totaltime)
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
@@ -15,8 +101,10 @@ class Command(BaseCommand):
             help='Do not profile MEDIA_URL and ADMIN_MEDIA_URL'),
         make_option('--use-cprofile', action='store_true', dest='use_cprofile', default=False,
             help='Use cProfile if available, this is disabled per default because of incompatibilities.'),
+        make_option('--kcachegrind', action='store_true', dest='use_lsprof', default=False,
+            help='Create kcachegrind compatible lsprof files, this requires and automatically enables cProfile.'),
     )
-    help = "Starts a lightweight Web server for development."
+    help = "Starts a lightweight Web server with profiling enabled."
     args = '[optional port number, or ipaddr:port]'
 
     # Validation is called explicitly each time the server is reloaded.
@@ -52,14 +140,19 @@ class Command(BaseCommand):
             from django.conf import settings
 
             import hotshot, time, os
-            if options.get('use_cprofile', False):
+            USE_CPROFILE = options.get('use_cprofile', False)
+            USE_LSPROF = options.get('use_lsprof', False)
+            if USE_LSPROF:
+               USE_CPROFILE = True
+            if USE_CPROFILE:
                 try:
                     import cProfile
                     USE_CPROFILE = True
                 except ImportError:
+                    print "cProfile disabled, module cannot be imported!"
                     USE_CPROFILE = False
-            else:
-                USE_CPROFILE = False
+            if USE_LSPROF and not USE_CPROFILE:
+        	raise SystemExit("Kcachegrind compatible output format required cProfile from Python 2.5")
             prof_path = options.get('prof_path', '/tmp')
             def make_profiler_handler(inner_handler):
                 def handler(environ, start_response):
@@ -79,7 +172,10 @@ class Command(BaseCommand):
                     try:
                         return prof.runcall(inner_handler, environ, start_response)
                     finally:
-                        if USE_CPROFILE:
+                        if USE_LSPROF:
+                            kg = KCacheGrind(prof)
+                            kg.output(file(profname, 'w'))
+                        elif USE_CPROFILE:
                             prof.dump_stats(profname)
                 return handler
 
