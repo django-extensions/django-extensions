22b5af8679f2ea8da57778a48ab70b33d9efefa4
trbs <trbs@trbs.net>
2010-11-25 14:38:49 +0100

    Ticket 50: Added proposed fix for UUIDField to allow it to be fixed as primary_key

--- django_extensions/db/fields/__init__.py
+++ django_extensions/db/fields/__init__.py
@@ -211,6 +211,15 @@ class UUIDField(CharField):
     def get_internal_type(self):
         return CharField.__name__
 
+    def contribute_to_class(self, cls, name):
+        if self.primary_key: 
+            assert not cls._meta.has_auto_field, "A model can't have more than one AutoField: %s %s %s; have %s" % (self,cls,name,cls._meta.auto_field)
+            super(UUIDField, self).contribute_to_class(cls, name)
+            cls._meta.has_auto_field = True
+            cls._meta.auto_field = self
+        else:
+            super(UUIDField, self).contribute_to_class(cls, name)
+
     def create_uuid(self):
         if not self.version or self.version==4:
             return uuid.uuid4()
