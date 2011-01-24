8255fbee388848947aeb70bc960e23b28c11a0dd
Antti Kaihola <akaihol+github@ambitone.com>
2011-01-24 18:31:31 +0800

    Fixes ticket #66: customize output colors only if colors supported

--- django_extensions/management/color.py
+++ django_extensions/management/color.py
@@ -7,8 +7,9 @@ from django.utils import termcolors
 
 def color_style():
     style = color.color_style()
-    style.URL = termcolors.make_style(fg='green', opts=('bold',))
-    style.MODULE = termcolors.make_style(fg='yellow')
-    style.MODULE_NAME = termcolors.make_style(opts=('bold',))
-    style.URL_NAME = termcolors.make_style(fg='red')
+    if color.supports_color():
+        style.URL = termcolors.make_style(fg='green', opts=('bold',))
+        style.MODULE = termcolors.make_style(fg='yellow')
+        style.MODULE_NAME = termcolors.make_style(opts=('bold',))
+        style.URL_NAME = termcolors.make_style(fg='red')
     return style
