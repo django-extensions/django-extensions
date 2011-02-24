ed706202928e67e82cdee36a41f77f8f7b7adc28
Trey Hunner <treyhunner@gmail.com>
2011-02-24 08:40:31 -0800

    Added keyword argument to AutoSlug field to allow non-unique slugs

--- django_extensions/db/fields/__init__.py
+++ django_extensions/db/fields/__init__.py
@@ -44,6 +44,7 @@ class AutoSlugField(SlugField):
             self._populate_from = populate_from
         self.separator = kwargs.pop('separator',  u'-')
         self.overwrite = kwargs.pop('overwrite', False)
+        self.allow_duplicates = kwargs.pop('allow_duplicates', False)
         super(AutoSlugField, self).__init__(*args, **kwargs)
 
     def _slug_strip(self, value):
@@ -77,7 +78,7 @@ class AutoSlugField(SlugField):
             # step from its number, clean-up
             slug = self._slug_strip(getattr(model_instance, self.attname))
             next = slug.split(self.separator)[-1]
-            if next.isdigit():
+            if next.isdigit() and not self.allow_duplicates:
                 slug = self.separator.join(slug.split(self.separator)[:-1])
                 next = int(next)
             else:
@@ -90,6 +91,9 @@ class AutoSlugField(SlugField):
             slug = slug[:slug_len]
         slug = self._slug_strip(slug)
         original_slug = slug
+
+        if self.allow_duplicates:
+            return slug
 
         # exclude the current model instance from the queryset used in finding
         # the next valid slug
