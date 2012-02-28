91d0022132d911fb8dedbb6446cb6648657b0e64
Simon Andersson <simon.andersson@keybroker.com>
2012-02-28 16:49:44 +0100

    Runserver: add quarter_hourly
    In addition to minutely, hourly etc, add quarter_hourly as an option for
    cron dispatch

--- django_extensions/management/signals.py
+++ django_extensions/management/signals.py
@@ -4,6 +4,7 @@ signals we use to trigger regular batch jobs
 from django.dispatch import Signal
 
 run_minutely_jobs = Signal()
+run_quarter_hourly_jobs = Signal()
 run_hourly_jobs = Signal()
 run_daily_jobs = Signal()
 run_weekly_jobs = Signal()
