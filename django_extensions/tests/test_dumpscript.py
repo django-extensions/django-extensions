c6a877c9e46ff26c38ef9ee4a5aae0c6e3823fef
Dexter Tad-y <dtady@cpan.org>
2012-04-10 00:03:39 +0800

    added inclusion of test models for test_dumpscript

--- django_extensions/tests/test_dumpscript.py
+++ django_extensions/tests/test_dumpscript.py
@@ -4,13 +4,22 @@ from django.test import TestCase
 from django.core.management import call_command
 from django_extensions.tests.models import Name
 
+from django.conf import settings
+from django.db.models import loading
+
 class DumpScriptTests(TestCase):
     def setUp(self):
         self.real_stdout = sys.stdout
         sys.stdout = StringIO()
 
+        settings.INSTALLED_APPS += ('django_extensions.tests',)
+        loading.cache.loaded = False
+        call_command('syncdb', verbosity=0)
+
     def tearDown(self):
         sys.stdout = self.real_stdout
+        settings.INSTALLED_APPS.pop()
+        loading.cache.loaded = False
 
     def test_runs(self):
         # lame test...does it run?
