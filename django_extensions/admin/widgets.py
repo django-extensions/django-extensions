1f03dd6480f369795d8620510b7defc6a29452e1
digitalpbk <arun.pbk@gmail.com>
2012-04-11 20:04:35 +0530

    ADMIN_MEDIA_PREFIX is deprecated in django 1.4

--- django_extensions/admin/widgets.py
+++ django_extensions/admin/widgets.py
@@ -39,7 +39,7 @@ class ForeignKeySearchInput(ForeignKeyRawIdWidget):
     def render(self, name, value, attrs=None):
         if attrs is None:
             attrs = {}
-        output = [super(ForeignKeySearchInput, self).render(name, value, attrs)]
+        #output = [super(ForeignKeySearchInput, self).render(name, value, attrs)]
         opts = self.rel.to._meta
         app_label = opts.app_label
         model_name = opts.object_name.lower()
@@ -57,10 +57,16 @@ class ForeignKeySearchInput(ForeignKeyRawIdWidget):
             label = self.label_for_value(value)
         else:
             label = u''
+            
+        try:
+            admin_media_prefix = settings.ADMIN_MEDIA_PREFIX
+        except AttributeError:
+            admin_media_prefix = settings.STATIC_URL + "admin/"
+        
         context = {
             'url': url,
             'related_url': related_url,
-            'admin_media_prefix': settings.ADMIN_MEDIA_PREFIX,
+            'admin_media_prefix': admin_media_prefix,
             'search_path': self.search_path,
             'search_fields': ','.join(self.search_fields),
             'model_name': model_name,
