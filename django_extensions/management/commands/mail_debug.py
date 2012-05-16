4bdd6804d5c4241632a259cbb33d9e40d8b49fee
Kris Kumler <kris@kumler.us>
2012-05-16 00:40:02 -0400

    Added file output and setting options to mail_debug command.

--- django_extensions/management/commands/mail_debug.py
+++ django_extensions/management/commands/mail_debug.py
@@ -1,10 +1,41 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 import sys
 import smtpd
 import asyncore
+from optparse import make_option
+
+
+# Modified from http://stackoverflow.com/a/616686/4281
+class Tee(object):
+    """ Intercept sys.stdout and redirect to a file, and stdout."""
+    def __init__(self, name, mode='w', tee=True):
+        self.tee = tee
+        self.file = open(name, mode)
+        self.stdout = sys.stdout
+        sys.stdout = self
+
+    def __del__(self):
+        sys.stdout = self.stdout
+        self.file.close()
+
+    def write(self, data):
+        """Intercept output and write to file, real stdout."""
+        self.file.write(data)
+        if self.tee:
+            self.stdout.write(data)
 
 
 class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--output', dest='output_file', default=None,
+            help='Specifies an output file to send all messages.'),
+        make_option('--tee', dest='tee', action='store_true', default=False,
+            help='Send output to file and stdout (requires --output).'),
+        make_option('--use-settings', dest='use_settings',
+            action='store_true', default=False,
+            help='Uses EMAIL_HOST and HOST_PORT from Django settings.'),
+
+    )
     help = "Starts a test mail server for development."
     args = '[optional port number or ippaddr:port]'
 
@@ -14,8 +45,13 @@ class Command(BaseCommand):
         if args:
             raise CommandError('Usage is runserver %s' % self.args)
         if not addrport:
-            addr = ''
-            port = '1025'
+            if options.get('use_settings', False):
+                from django.conf import settings
+                addr = settings.EMAIL_HOST
+                port = settings.EMAIL_PORT
+            else:
+                addr = ''
+                port = '1025'
         else:
             try:
                 addr, port = addrport.split(':')
@@ -33,6 +69,11 @@ class Command(BaseCommand):
 
         def inner_run():
             print "Now accepting mail at %s:%s" % (addr, port)
+            # Set up output redirection after the above info message.
+            if options.get('output_file', None):
+                self.tee = Tee(options.get('output_file', None),
+                        tee=options.get('tee', False))
+
             server = smtpd.DebuggingServer((addr, port), None)
             asyncore.loop()
 
