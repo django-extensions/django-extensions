95be43a7b1e135e711879ce8f13cb4df27b7917a
Marc Abramowitz <marc@marc-abramowitz.com>
2012-06-12 22:50:49 -0700

    Make `python setup.py test` run the test suite.

--- setup.py
+++ setup.py
@@ -88,6 +88,8 @@ additions for Django projects. See the project page for more information:
     packages=packages,
     cmdclass=cmdclasses,
     data_files=data_files,
+    tests_require=['Django', 'unittest2'],
+    test_suite='unittest2.collector',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Development Status :: 5 - Production/Stable',
