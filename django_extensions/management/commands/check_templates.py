98e98ec9c0f4215aa7594f4912d46ed705cdaa88
trbs <trbs@trbs.net>
2012-06-19 18:37:55 +0200

    check_templates; add support for CHECK_TEMPLATES_EXTRA_TEMPLATE_DIRS in settings file

--- django_extensions/management/commands/check_templates.py
+++ django_extensions/management/commands/check_templates.py
@@ -21,7 +21,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         style = color_style()
         from django.conf import settings
-        template_dirs = set(settings.TEMPLATE_DIRS) | set(options.get('includes', []))
+        template_dirs = set(settings.TEMPLATE_DIRS)
+        template_dirs |= set(options.get('includes', []))
+        template_dirs |= set(getattr(settings, 'CHECK_TEMPLATES_EXTRA_TEMPLATE_DIRS', []))
         settings.TEMPLATE_DIRS = list(template_dirs)
         verbosity = int(options.get('verbosity', 1))
         for template_dir in template_dirs:
