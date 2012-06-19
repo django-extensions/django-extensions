5b056cfbf40825c1bf361894a30ec112d7b709c5
trbs <trbs@trbs.net>
2012-06-19 18:37:55 +0200

    check_templates; add support for CHECK_TEMPLATES_EXTRA_TEMPLATE_DIRS in settings file

--- docs/check_templates.py
+++ docs/check_templates.py
@@ -6,11 +6,27 @@ check_templates
 Options
 =======
 
-verbosity; A higher verbosity level will print out all the files that are processed not just the onces that contain errors.
+verbosity
+~~~~~~~~~
+A higher verbosity level will print out all the files that are processed not just the onces that contain errors.
 
-break: Do not continue scanning other templates after the first failure.
+break
+~~~~~
+Do not continue scanning other templates after the first failure.
 
-includes: Use -i (can be used multiple times) to add directories to the TEMPLATE_DIRS.
+includes
+~~~~~~~~
+Use -i (can be used multiple times) to add directories to the TEMPLATE_DIRS.
+
+Settings
+========
+
+CHECK_TEMPLATES_EXTRA_TEMPLATE_DIRS
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+You can use `CHECK_TEMPLATES_EXTRA_TEMPLATE_DIRS` to include a number of template dirs per default directly from the settings file.
+This can be usefull for situations where TEMPLATE_DIRS is dynamically generated or switched in middleware or you have other template
+dirs for external applications like celery you want to check as well.
 
 How
 ===
