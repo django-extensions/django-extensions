c9fd8ccf301cebc784b602a0af214eef67f80548
trbs <trbs@trbs.net>
2012-07-21 02:27:30 +0200

    create test keys and directory for keyczar so that it does not fail the tests

--- django_extensions/tests/encrypted_fields.py
+++ django_extensions/tests/encrypted_fields.py
@@ -1,15 +1,13 @@
-import unittest
-
-
 from django.db import connection
 from django.conf import settings
 from django.core.management import call_command
 from django.db.models import loading
+from django.utils import unittest
 
 # Only perform encrypted fields tests if keyczar is present
 # Resolves http://github.com/django-extensions/django-extensions/issues/#issue/17
 try:
-    from keyczar import keyczar
+    from keyczar import keyczar, keyczart, keyinfo
     from django_extensions.tests.models import Secret
     from django_extensions.db.fields.encrypted import EncryptedTextField, EncryptedCharField
     keyczar_active = True
@@ -73,3 +71,4 @@ class EncryptedFieldsTestCase(unittest.TestCase):
         secret = Secret.objects.create(text=test_val)
         retrieved_secret = Secret.objects.get(id=secret.id)
         self.assertEqual(test_val, retrieved_secret.text)
+
--- run_tests.py
+++ run_tests.py
@@ -1,40 +1,58 @@
 
 import sys
+import shutil
+import tempfile
 from django.conf import settings
 from django.core.management import call_command
 
 def main():
     # Dynamically configure the Django settings with the minimum necessary to
     # get Django running tests
-    settings.configure(
-        INSTALLED_APPS = [
-            'django.contrib.auth',
-            'django.contrib.contenttypes',
-            'django.contrib.admin',
-            'django.contrib.sessions',
-            'django_extensions',
-            'django_extensions.tests',
-        ],
-        # Django replaces this, but it still wants it. *shrugs*
-        DATABASE_ENGINE = 'django.db.backends.sqlite3',
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-            }
-        },
-        MEDIA_ROOT = '/tmp/django_extensions_test_media/',
-        MEDIA_PATH = '/media/',
-        ROOT_URLCONF = 'django_extensions.tests.urls',
-        DEBUG = True,
-        TEMPLATE_DEBUG = True
-    )
+    ENCRYPTED_FIELD_KEYS_DIR = None
+    try:
+        try:
+            from keyczar import keyczart, keyinfo
+            ENCRYPTED_FIELD_KEYS_DIR = tempfile.mkdtemp("django_extensions_tests_keyzcar_keys_dir")
+            # TODO: move this to the unit test for encrypted fields
+            keyczart.Create(ENCRYPTED_FIELD_KEYS_DIR, "test", keyinfo.DECRYPT_AND_ENCRYPT)
+            keyczart.AddKey(ENCRYPTED_FIELD_KEYS_DIR, "PRIMARY")
+        except ImportError:
+            pass
 
-    from django.test.utils import get_runner
-    test_runner = get_runner(settings)(verbosity=2, interactive=True)
-    failures = test_runner.run_tests(['django_extensions'])
-    sys.exit(failures)
+        settings.configure(
+            INSTALLED_APPS = [
+                'django.contrib.auth',
+                'django.contrib.contenttypes',
+                'django.contrib.admin',
+                'django.contrib.sessions',
+                'django_extensions',
+                'django_extensions.tests',
+            ],
+            # Django replaces this, but it still wants it. *shrugs*
+            DATABASE_ENGINE = 'django.db.backends.sqlite3',
+            DATABASES = {
+                'default': {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                }
+            },
+            MEDIA_ROOT = '/tmp/django_extensions_test_media/',
+            MEDIA_PATH = '/media/',
+            ROOT_URLCONF = 'django_extensions.tests.urls',
+            DEBUG = True,
+            TEMPLATE_DEBUG = True,
+            ENCRYPTED_FIELD_KEYS_DIR = ENCRYPTED_FIELD_KEYS_DIR,
+        )
+
+        from django.test.utils import get_runner
+        test_runner = get_runner(settings)(verbosity=2, interactive=True)
+        failures = test_runner.run_tests(['django_extensions'])
+        sys.exit(failures)
+
+    finally:
+        if ENCRYPTED_FIELD_KEYS_DIR:
+            # cleanup crypto key temp dir
+            shutil.rmtree(ENCRYPTED_FIELD_KEYS_DIR)
 
 
 if __name__ == '__main__':
     main()
-
