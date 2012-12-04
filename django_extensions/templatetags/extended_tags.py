054d44d3207a5103d16f61ce503b1f4d3a96d698
Paulo R. Macedo Chaves <proberto.macedo@gmail.com>
2012-12-03 22:46:02 -0200

    Add jsonify template tag

--- django_extensions/templatetags/extended_tags.py
+++ django_extensions/templatetags/extended_tags.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+from django.core.serializers import serialize
+from django.db.models.query import QuerySet
+from django.utils import simplejson
 from django.template import Library, Node, TemplateSyntaxError
 
 
 register = Library()
+
+
+def jsonify(object):
+    if isinstance(object, QuerySet):
+        return serialize('json', object)
+    return simplejson.dumps(object)
+
+
+register.filter('jsonify', jsonify)
 
 
 class SplitListNode(Node):
