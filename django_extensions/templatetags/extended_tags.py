68dd2c24e816960ae35278872f9f593279e5610c
Paulo R. Macedo Chaves <proberto.macedo@gmail.com>
2012-12-03 23:52:37 -0200

    Add verbatim template tag, present in django 1.5

--- django_extensions/templatetags/extended_tags.py
+++ django_extensions/templatetags/extended_tags.py
@@ -5,9 +5,38 @@ from django.core.serializers import serialize
 from django.db.models.query import QuerySet
 from django.utils import simplejson
 from django.template import Library, Node, TemplateSyntaxError
+from django import template
 
 
 register = Library()
+
+
+class VerbatimNode(template.Node):
+
+    def __init__(self, text):
+        self.text = text
+
+    def render(self, context):
+        return self.text
+
+
+@register.tag
+def verbatim(parser, token):
+    text = []
+    while 1:
+        token = parser.tokens.pop(0)
+        if token.contents == 'endverbatim':
+            break
+        if token.token_type == template.TOKEN_VAR:
+            text.append('{{')
+        elif token.token_type == template.TOKEN_BLOCK:
+            text.append('{%')
+        text.append(token.contents)
+        if token.token_type == template.TOKEN_VAR:
+            text.append('}}')
+        elif token.token_type == template.TOKEN_BLOCK:
+            text.append('%}')
+    return VerbatimNode(''.join(text))
 
 
 def jsonify(object):
