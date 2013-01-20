efbea8320518d94c8c7eeb23a90d8e97a4727fb6
trbs <trbs@trbs.net>
2013-01-20 18:03:04 +0100

    make sure test can find the test-management-command

--- django_extensions/tests/management_command.py
+++ django_extensions/tests/management_command.py
@@ -27,14 +27,20 @@ class MockLoggingHandler(logging.Handler):
 
 
 class CommandTest(TestCase):
-
     def test_error_logging(self):
         # Ensure command errors are properly logged and reraised
         from django_extensions.management.base import logger
         logger.addHandler(MockLoggingHandler())
+        from django.conf import settings
+        org_apps = None
+        apps = list(settings.INSTALLED_APPS)
+        if not 'django_extensions.tests' in apps:
+            apps.append('django_extensions.tests')
         self.assertRaises(Exception, call_command, 'error_raising_command')
         handler = logger.handlers[0]
         self.assertEqual(len(handler.messages['error']), 1)
+        if org_apps:
+            settings.INSTALLED_APPS = org_apps
 
 
 class ShowTemplateTagsTests(TestCase):
