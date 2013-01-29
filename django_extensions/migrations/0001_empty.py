d2a7402a36fd07a460b38f71bb7b7a35501a4ea8
trbs <trbs@trbs.net>
2013-01-29 17:32:27 +0100

    add empty migration for south so it does not complain about django_extensions anymore

---
+++ django_extensions/migrations/0001_empty.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        pass
+
+    def backwards(self, orm):
+        pass
+
+    models = {
+        
+    }
+
+    complete_apps = ['django_extensions']
