540f3674dd0befb4830def664c6ab96349a4420e
Florian Hahn <flo@fhahn.com>
2013-02-24 23:02:58 +0100

    convert doctests to unittests and only run uuid tests if uuid is not shipped with python (version < 2.5)

--- django_extensions/tests/__init__.py
+++ django_extensions/tests/__init__.py
@@ -1,19 +1,14 @@
 from django.db import models  # NOQA
 from django_extensions.tests.test_dumpscript import DumpScriptTests
-from django_extensions.tests.utils import UTILS_TRUNCATE_LETTERS_TESTS
-from django_extensions.tests.utils import UTILS_UUID_TESTS
+from django_extensions.tests.utils import TruncateLetterTests
 from django_extensions.tests.json_field import JsonFieldTest
 from django_extensions.tests.uuid_field import UUIDFieldTest
 from django_extensions.tests.fields import AutoSlugFieldTest
 from django_extensions.tests.management_command import CommandTest, ShowTemplateTagsTests
 
-__test__ = {
-    'UTILS_TRUNCATE_LETTERS_TESTS': UTILS_TRUNCATE_LETTERS_TESTS,
-    'UTILS_UUID_TESTS': UTILS_UUID_TESTS,
-}
 
 __test_classes__ = [
-    DumpScriptTests, JsonFieldTest, UUIDFieldTest, AutoSlugFieldTest, CommandTest, ShowTemplateTagsTests
+    DumpScriptTests, JsonFieldTest, UUIDFieldTest, AutoSlugFieldTest, CommandTest, ShowTemplateTagsTests, TruncateLetterTests
 ]
 
 try:
@@ -22,4 +17,3 @@ try:
     __test_classes__.append(EncryptedFieldsTestCase)
 except ImportError:
     pass
-
