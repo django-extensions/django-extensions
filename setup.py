d6d5ab076dd74da73af880f232a165f572964986
trbs <trbs@trbs.net>
2013-02-27 23:01:24 +0100

    Add Python 3 Trove classifier ("Programming Language :: Python :: 3") to setup.py, refs #288

--- setup.py
+++ setup.py
@@ -99,6 +99,7 @@ additions for Django projects. See the project page for more information:
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Topic :: Utilities',
     ],
 )
