7b1fcfd88765e5cd5f8acdce1c89a12186f47c9a
trbs <trbs@trbs.net>
2013-02-28 17:50:27 +0100

    use six.reraise

--- django_extensions/management/technical_response.py
+++ django_extensions/management/technical_response.py
@@ -1,10 +1,7 @@
-# flake8: noqa
-
+import six
 import sys
 
 
 def null_technical_500_response(request, exc_type, exc_value, tb):
-    if sys.version_info.major < 3:
-        raise exc_type, exc_value, tb
-    else:
-        raise exc_type(exc_value).with_traceback(tb)
+    six.reraise(exc_type, exc_value, tb)
+
