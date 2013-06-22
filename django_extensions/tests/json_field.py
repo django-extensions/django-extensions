53419afa42929e1019f58b38b2ba39c4ef3131a8
Ryan Kaskel <dev@ryankaskel.com>
2013-06-22 11:50:18 +0100

    Python 3 metaclass compatibility for Field subclasses.

--- django_extensions/tests/json_field.py
+++ django_extensions/tests/json_field.py
@@ -27,6 +27,10 @@ class JsonFieldTest(unittest.TestCase):
         j = TestModel.objects.create(a=6, j_field=dict(foo='bar'))
         self.assertEqual(j.a, 6)
 
+    def testDefault(self):
+        j = TestModel.objects.create(a=1)
+        self.assertEqual(j.j_field, {})
+
     def testEmptyList(self):
         j = TestModel.objects.create(a=6, j_field=[])
         self.assertTrue(isinstance(j.j_field, list))
