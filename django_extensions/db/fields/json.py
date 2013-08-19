d5c87fc20703805c7d05597bc4e6f8d956ef4a41
Camilo Nova <camilo.nova@gmail.com>
2013-08-19 09:48:57 -0500

    Use DjangoJSONEncoder instead of a custom Encoder
    It's better to use built-in django JSON encoder, it's there since django 1.3 so doesn't need backwards compatibility code.
    
    Fixes #324

--- django_extensions/db/fields/json.py
+++ django_extensions/db/fields/json.py
@@ -16,20 +16,11 @@ from decimal import Decimal
 from django.db import models
 from django.conf import settings
 from django.utils import simplejson
-
-
-class JSONEncoder(simplejson.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Decimal):
-            return str(obj)
-        elif isinstance(obj, datetime.datetime):
-            assert settings.TIME_ZONE == 'UTC'
-            return obj.strftime('%Y-%m-%dT%H:%M:%SZ')
-        return simplejson.JSONEncoder.default(self, obj)
+from django.core.serializers.json import DjangoJSONEncoder
 
 
 def dumps(value):
-    return JSONEncoder().encode(value)
+    return DjangoJSONEncoder().encode(value)
 
 
 def loads(txt):
