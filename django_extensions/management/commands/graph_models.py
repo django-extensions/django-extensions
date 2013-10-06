f5e0b0d1f3112be8de84750d9c4c1e0116d7e827
trbs <trbs@trbs.net>
2013-10-06 21:38:35 +0200

    improvements to graph_models
    - better use of django templates so one can reasonably overwrite dot file generation
    - sort items and make sure primary_key and foreignkeys are always on top
    - make database relations (primary key and foreignkey) more clear
    - do not hide foreign keys from attribute list (added option for if you want to hide this)
    - allow to specify default values in settings file for graph_models command

--- django_extensions/management/commands/graph_models.py
+++ django_extensions/management/commands/graph_models.py
@@ -1,5 +1,7 @@
+import sys
+from optparse import make_option, NO_DEFAULT
 from django.core.management.base import BaseCommand, CommandError
-from optparse import make_option
+from django.conf import settings
 from django_extensions.management.modelviz import generate_dot
 
 
@@ -17,7 +19,7 @@ except ImportError:
 
 
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
+    graph_models_options = (
         make_option('--pygraphviz', action='store_true', dest='pygraphviz',
                     help='Use PyGraphViz to generate the image.'),
         make_option('--pydot', action='store_true', dest='pydot',
@@ -42,9 +44,14 @@ class Command(BaseCommand):
                     help='Exclude specific model(s) from the graph. Can also load exclude list from file.'),
         make_option('--inheritance', '-e', action='store_true', dest='inheritance',
                     help='Include inheritance arrows'),
+        make_option('--hide-relations-from-fields', '-R', action='store_false', dest="relations_as_fields",
+                    default=True, help="Do not show relations as fields in the graph."),
+        make_option('--disable-sort-fields', '-S', action="store_false", dest="sort_fields",
+                    default=True, help="Do not sort fields"),
     )
+    option_list = BaseCommand.option_list + graph_models_options
 
-    help = ("Creates a GraphViz dot file for the specified app names.  You can pass multiple app names and they will all be combined into a single model.  Output is usually directed to a dot file.")
+    help = "Creates a GraphViz dot file for the specified app names.  You can pass multiple app names and they will all be combined into a single model.  Output is usually directed to a dot file."
     args = "[appname]"
     label = 'application name'
 
@@ -52,12 +59,16 @@ class Command(BaseCommand):
     can_import_settings = True
 
     def handle(self, *args, **options):
+        self.options_from_settings(options)
+
         if len(args) < 1 and not options['all_applications']:
             raise CommandError("need one or more arguments for appname")
 
         use_pygraphviz = options.get('pygraphviz', False)
         use_pydot = options.get('pydot', False)
-        dotdata = generate_dot(args, **options)
+        cli_options = ' '.join(sys.argv[2:])
+        dotdata = generate_dot(args, cli_options=cli_options, **options)
+        dotdata = dotdata.encode('utf-8')
         if options['outputfile']:
             if not use_pygraphviz and not use_pydot:
                 if HAS_PYGRAPHVIZ:
@@ -73,28 +84,41 @@ class Command(BaseCommand):
         else:
             self.print_output(dotdata)
 
+    def options_from_settings(self, options):
+        defaults = getattr(settings, 'GRAPH_MODELS', None)
+        if defaults:
+            for option in self.graph_models_options:
+                long_opt = option._long_opts[0]
+                if long_opt:
+                    long_opt = long_opt.lstrip("-").replace("-", "_")
+                    if long_opt in defaults:
+                        default_value = None
+                        if not option.default == NO_DEFAULT:
+                            default_value = option.default
+                        if options[option.dest] == default_value:
+                            options[option.dest] = defaults[long_opt]
+
     def print_output(self, dotdata):
-        print(dotdata.encode('utf-8'))
+        print(dotdata)
 
     def render_output_pygraphviz(self, dotdata, **kwargs):
         """Renders the image using pygraphviz"""
         if not HAS_PYGRAPHVIZ:
             raise CommandError("You need to install pygraphviz python module")
 
-        vizdata = ' '.join(dotdata.split("\n")).strip().encode('utf-8')
         version = pygraphviz.__version__.rstrip("-svn")
         try:
             if tuple(int(v) for v in version.split('.')) < (0, 36):
                 # HACK around old/broken AGraph before version 0.36 (ubuntu ships with this old version)
                 import tempfile
                 tmpfile = tempfile.NamedTemporaryFile()
-                tmpfile.write(vizdata)
+                tmpfile.write(dotdata)
                 tmpfile.seek(0)
-                vizdata = tmpfile.name
+                dotdata = tmpfile.name
         except ValueError:
             pass
 
-        graph = pygraphviz.AGraph(vizdata)
+        graph = pygraphviz.AGraph(dotdata)
         graph.layout(prog=kwargs['layout'])
         graph.draw(kwargs['outputfile'])
 
@@ -103,8 +127,9 @@ class Command(BaseCommand):
         if not HAS_PYDOT:
             raise CommandError("You need to install pydot python module")
 
-        vizdata = ' '.join(dotdata.split("\n")).strip().encode('utf-8')
-        graph = pydot.graph_from_dot_data(vizdata)
+        graph = pydot.graph_from_dot_data(dotdata)
+        if not graph:
+            raise CommandError("pydot returned an error")
         output_file = kwargs['outputfile']
         formats = ['bmp', 'canon', 'cmap', 'cmapx', 'cmapx_np', 'dot', 'dia', 'emf',
                    'em', 'fplus', 'eps', 'fig', 'gd', 'gd2', 'gif', 'gv', 'imap',
