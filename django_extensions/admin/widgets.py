f3ceacb89dfa6a55aee7a51d6d03c78166ce547c
Camilo Nova <camilo.nova@axiacore.com>
2014-01-11 10:49:02 -0500

    Use a newer jquery version if django <= 1.5.x. Fixes #398

--- django_extensions/admin/widgets.py
+++ django_extensions/admin/widgets.py
@@ -1,4 +1,5 @@
 import six
+import django
 
 from django import forms
 from django.contrib.admin.sites import site
@@ -22,6 +23,12 @@ class ForeignKeySearchInput(ForeignKeyRawIdWidget):
         js_files = ['django_extensions/js/jquery.bgiframe.min.js',
                     'django_extensions/js/jquery.ajaxQueue.js',
                     'django_extensions/js/jquery.autocomplete.js']
+
+        # Use a newer version of jquery if django version <= 1.5.x
+        # When removing this compatibility code also remove jquery-1.7.2.min.js file.
+        if int(django.get_version()[2]) <= 5:
+            js_files.insert(0, 'django_extensions/js/jquery-1.7.2.min.js')
+
         return forms.Media(css={'all': ('django_extensions/css/jquery.autocomplete.css',)},
                            js=js_files)
 
