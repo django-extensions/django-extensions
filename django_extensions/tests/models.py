4d8beed57ed6dc5236308a21f52d3886efc959a6
Chris Forrette <chris@chrisforrette.com>
2014-02-04 23:49:08 -0800

    Adding a 'Post' ActivatorModel test model, adding test_models.py with ActivatorModel tests

--- django_extensions/tests/models.py
+++ django_extensions/tests/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django_extensions.db.models import ActivatorModel
+
 
 class Secret(models.Model):
     name = models.CharField(blank=True, max_length=255, null=True)
@@ -19,3 +21,7 @@ class Person(models.Model):
     age = models.PositiveIntegerField()
     children = models.ManyToManyField('self')
     notes = models.ManyToManyField(Note)
+
+
+class Post(ActivatorModel):
+    title = models.CharField(max_length=255)
