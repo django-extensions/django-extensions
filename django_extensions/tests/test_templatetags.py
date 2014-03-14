62aedeeb3147dffbf2909c5dbf271050ce55146b
trbs <trbs@trbs.net>
2014-03-14 18:53:46 +0100

    fix from #481 for Python 3.2

--- django_extensions/tests/test_templatetags.py
+++ django_extensions/tests/test_templatetags.py
@@ -1,3 +1,5 @@
+import six
+
 from django.test import TestCase
 
 from django_extensions.templatetags.widont import widont, widont_html
@@ -6,8 +8,8 @@ from django_extensions.templatetags.widont import widont, widont_html
 class TemplateTagsTests(TestCase):
     def test_widont(self):
         widont('Test Value')
-        widont(u'Test Value')
+        widont(six.u('Test Value'))
 
     def test_widont_html(self):
         widont_html('Test Value')
-        widont_html(u'Test Value')
+        widont_html(six.u'Test Value'))
