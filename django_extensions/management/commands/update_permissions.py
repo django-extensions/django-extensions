1ae401346ea9c0c4ad7616505828c676cb115099
Alex Hayes <alex.hayes@roi.com.au>
2014-03-27 13:23:10 +1100

    Add pre/post command signals to hook into operation of commands.

--- django_extensions/management/commands/update_permissions.py
+++ django_extensions/management/commands/update_permissions.py
@@ -1,13 +1,15 @@
 from django.core.management.base import BaseCommand
 from django.db.models import get_models, get_app
 from django.contrib.auth.management import create_permissions
-
+from django_extensions.management.signals import pre_command, post_command
 
 class Command(BaseCommand):
     args = '<app app ...>'
     help = 'reloads permissions for specified apps, or all apps if no args are specified'
 
     def handle(self, *args, **options):
+        pre_command.send(self.__class__)
+        
         if not args:
             apps = []
             for model in get_models():
@@ -19,3 +21,4 @@ class Command(BaseCommand):
         for app in apps:
             create_permissions(app, get_models(), int(options.get('verbosity', 0)))
 
+        post_command.send(self.__class__)
