624011f0bf1606d9a918bd002c77f9c80a1736d4
trbs <trbs@trbs.net>
2014-05-01 23:29:22 +0200

    shortuuid is also required to run tests

--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ additions for Django projects. See the project page for more information:
     cmdclass=cmdclasses,
     package_data=package_data,
     install_requires=['six>=1.2'],
-    tests_require=['Django'],
+    tests_require=['Django', 'shortuuid'],
     test_suite='run_tests.main',
     classifiers=[
         'Development Status :: 4 - Beta',
