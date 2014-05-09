3a81a744668c90dca47d6af4c96d03cb4f5bcb6c
Jaap Roes <jaap@eight.nl>
2014-05-09 11:23:30 +0200

    Reinstated clean_pyc and added tests for both clean_pyc and compile_pyc.

--- django_extensions/management/commands/clean_pyc.py
+++ django_extensions/management/commands/clean_pyc.py
@@ -1,6 +1,5 @@
 import fnmatch
 import os
-import py_compile
 from django.core.management.base import NoArgsCommand
 from django_extensions.management.utils import get_project_root
 from optparse import make_option
@@ -9,10 +8,13 @@ from os.path import join as _j
 
 class Command(NoArgsCommand):
     option_list = NoArgsCommand.option_list + (
+        make_option('--optimize', '-o', '-O', action='store_true',
+                    dest='optimize',
+                    help='Remove optimized python bytecode files'),
         make_option('--path', '-p', action='store', dest='path',
                     help='Specify path to recurse into'),
     )
-    help = "Compile python bytecode files for the project."
+    help = "Removes all python bytecode compiled files from the project."
 
     requires_model_validation = False
 
@@ -25,10 +27,11 @@ class Command(NoArgsCommand):
                 self.stdout.write(
                     "No path specified, assuming %s is the project root.\n"
                     % project_root)
+        exts = options.get("optimize", False) and "*.py[co]" or "*.pyc"
 
         for root, dirs, filenames in os.walk(project_root):
-            for filename in fnmatch.filter(filenames, '*.py'):
+            for filename in fnmatch.filter(filenames, exts):
                 full_path = _j(root, filename)
                 if verbosity > 1:
-                    self.stdout.write("Compiling %s...\n" % full_path)
-                py_compile.compile(full_path)
+                    self.stdout.write("%s\n" % full_path)
+                os.remove(full_path)
