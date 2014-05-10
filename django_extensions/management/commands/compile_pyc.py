154ffeaade9be366a05ab4e7e4e16a8f248e0799
trbs <trbs@trbs.net>
2014-05-11 01:57:33 +0200

    use BASE_DIR when available and add deprecation warning for our auto detection of project root

--- django_extensions/management/commands/compile_pyc.py
+++ django_extensions/management/commands/compile_pyc.py
@@ -1,7 +1,10 @@
-import fnmatch
 import os
+import time
+import fnmatch
+import warnings
 import py_compile
 from django.core.management.base import NoArgsCommand
+from django.conf import settings
 from django_extensions.management.utils import get_project_root
 from optparse import make_option
 from os.path import join as _j
@@ -18,13 +21,27 @@ class Command(NoArgsCommand):
 
     def handle_noargs(self, **options):
         project_root = options.get("path", None)
+        if not project_root:
+            project_root = getattr(settings, 'BASE_DIR', None)
+
         verbosity = int(options.get("verbosity"))
         if not project_root:
+            warnings.warn("settings.BASE_DIR or specifying --path will become mandatory in 1.4.0", DeprecationWarning)
             project_root = get_project_root()
             if verbosity > 0:
-                self.stdout.write(
-                    "No path specified, assuming %s is the project root.\n"
-                    % project_root)
+                self.stdout.write("""No path specified and settings.py does not contain BASE_DIR.
+Assuming '%s' is the project root.
+
+Please add BASE_DIR to your settings.py future versions 1.4.0 and higher of Django-Extensions
+will require either BASE_DIR or specifying the --path option.
+
+Waiting for 30 seconds. Press ctrl-c to abort.
+""" % project_root)
+                try:
+                    time.sleep(30)
+                except KeyboardInterrupt:
+                    self.stdout.write("Aborted\n")
+                    return
 
         for root, dirs, filenames in os.walk(project_root):
             for filename in fnmatch.filter(filenames, '*.py'):
