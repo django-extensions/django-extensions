a4efdb71c2c067af52d871711632eba0c06dc811
Ceesjan Luiten <ceesjan@ytec.nl>
2014-08-31 19:59:17 +0200

    Use Django's VERSION to determine which cleanup command to call

--- django_extensions/jobs/daily/daily_cleanup.py
+++ django_extensions/jobs/daily/daily_cleanup.py
@@ -13,7 +13,9 @@ class Job(DailyJob):
 
     def execute(self):
         from django.core import management
-        try:
-            management.call_command('clearsessions')
-        except management.CommandError:
-            management.call_command("cleanup")  # Django <1.5
+        from django import VERSION
+
+        if VERSION[:2] < (1, 5):
+            management.call_command("cleanup")
+        else:
+            management.call_command("clearsessions")
