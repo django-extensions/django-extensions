b8ee0845ec6faf7f29819fb79d419cf36debd921
NotSqrt <notsqrt@gmail.com>
2015-03-19 17:23:25 +0100

    Fix runscript with AppConfig apps
    with tests.
    
    Fixes #641

---
+++ tests/testapp/apps.py
@@ -0,0 +1,8 @@
+try:
+    from django.apps import AppConfig
+
+    class TestAppConfig(AppConfig):
+        name = 'tests.testapp'
+
+except ImportError:
+    pass
---
+++ tests/testapp/scripts/sample_script.py
@@ -0,0 +1,2 @@
+def run():
+    pass
