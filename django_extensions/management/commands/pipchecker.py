6bf8f3eb9a2b20a6844c258d79ccdc04fcd98df5
Kevin Ndung'u <kevgathuku@gmail.com>
2015-04-27 16:04:01 +0300

    Resolve issue #663
    Convert req["url"] to string before calling startswith method on it

--- django_extensions/management/commands/pipchecker.py
+++ django_extensions/management/commands/pipchecker.py
@@ -177,7 +177,7 @@ class Command(NoArgsCommand):
 
         """
         for name, req in list(self.reqs.items()):
-            req_url = req["url"]
+            req_url = str(req["url"])
             if not req_url:
                 continue
             if req_url.startswith("git") and "github.com/" not in req_url:
