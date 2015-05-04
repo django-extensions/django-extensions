c9fa00c6ca6ee4182305fcbf1a261d2f8221f2d9
trbs <trbs@trbs.net>
2015-05-04 08:48:32 +0200

    add simple rate limiter for logging

---
+++ django_extensions/logging/filters.py
@@ -0,0 +1,22 @@
+import time
+import logging
+from hashlib import md5
+
+class RateLimiterFilter(logging.Filter):
+    def filter(self, record):
+        from django.conf import settings
+        from django.core.cache import cache
+
+        # Rate is specified as 1 messages logged per N seconds. (aka cache timeout)
+        rate = getattr(settings, 'RATE_LIMITER_FILTER_RATE', 10)
+        prefix = getattr(settings, 'RATE_LIMITER_FILTER_PREFIX', 'ratelimiterfilter')
+
+        subject = record.getMessage()
+        cache_key = "ratelimiterfilter:%s" % md5(subject).hexdigest()
+
+        value = cache.get(cache_key)
+        if value:
+            return False
+
+        cache.set(cache_key, time.time(), rate)
+        return True
