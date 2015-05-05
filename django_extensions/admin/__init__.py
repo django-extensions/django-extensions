0df68380fa60ea9bdcba5f54c7c63e761c9ea7d0
Jonathan Tien <jonathan.tien@gmail.com>
2015-05-04 21:48:24 -0700

    update module_name to model_name
    in Django 1.8, the alias module_name was removed, the correct usage is model_name

--- django_extensions/admin/__init__.py
+++ django_extensions/admin/__init__.py
@@ -60,7 +60,7 @@ class ForeignKeyAutocompleteAdmin(ModelAdmin):
                 return self.admin_site.admin_view(view)(*args, **kwargs)
             return update_wrapper(wrapper, view)
 
-        info = self.model._meta.app_label, self.model._meta.module_name
+        info = self.model._meta.app_label, self.model._meta.model_name
 
         urlpatterns = patterns('', url(r'foreignkey_autocomplete/$', wrap(self.foreignkey_autocomplete), name='%s_%s_autocomplete' % info))
         urlpatterns += super(ForeignKeyAutocompleteAdmin, self).get_urls()
