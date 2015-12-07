244879a7f9c2502e65c1f5b5b4bb03fa6cd6f720
Levi Cameron <levi.cameron@alliancesoftware.com.au>
2015-12-07 16:14:33 +1100

    Better emulate read_default_file DB config parsing
    Fixes #773

--- django_extensions/management/commands/drop_test_database.py
+++ django_extensions/management/commands/drop_test_database.py
@@ -3,9 +3,10 @@ from optparse import make_option
 
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
-from six.moves import configparser, input
+from six.moves import input
 
 from django_extensions.management.utils import signalcommand
+from ._private import parse_mysql_settings
 
 try:
     from django.db.backends.base.creation import TEST_DATABASE_PREFIX
@@ -50,15 +51,9 @@ class Command(BaseCommand):
 
         engine = dbinfo.get('ENGINE').split('.')[-1]
 
-        user = password = database_name = ''
+        user = password = database_name = database_host = database_port = ''
         if engine == 'mysql':
-            read_default_file = dbinfo.get('OPTIONS', {}).get('read_default_file')
-            if read_default_file:
-                config = configparser.ConfigParser()
-                config.read(read_default_file)
-                user = config.get('client', 'user')
-                password = config.get('client', 'password')
-                database_name = config.get('client', 'database')
+            (user, password, database_name, database_host, database_port) = Command.parse_mysql_settings(options)
 
         user = options.get('user') or dbinfo.get('USER') or user
         password = options.get('password') or dbinfo.get('PASSWORD') or password
@@ -74,8 +69,8 @@ class Command(BaseCommand):
         if database_name is None or database_name == '':
             raise CommandError("You need to specify DATABASE_NAME in your Django settings file.")
 
-        database_host = dbinfo.get('HOST')
-        database_port = dbinfo.get('PORT')
+        database_host = dbinfo.get('HOST') or database_host
+        database_port = dbinfo.get('PORT') or database_port
 
         verbosity = int(options.get('verbosity', 1))
         if options.get('interactive'):
