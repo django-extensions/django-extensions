23ed333ec7eb56d187274bf5e79692a987be9f10
trbs <trbs@trbs.net>
2016-04-28 15:31:08 +0200

    add support classifier for python 2

--- setup.py
+++ setup.py
@@ -135,6 +135,7 @@ additions for Django projects. See the project page for more information:
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Utilities',
