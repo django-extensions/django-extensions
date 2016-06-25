27b268ac65c897257db66fee0eb17af65a85aa13
trbs <trbs@trbs.net>
2016-06-24 20:44:45 -0400

    add todo about the templatetags test not having decent test coverage

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -5,11 +5,12 @@ from django.test import TestCase
 from django_extensions.templatetags.widont import widont, widont_html
 
 
+# TODO: these tests are far from having decent test coverage
 class TemplateTagsTests(TestCase):
     def test_widont(self):
-        widont('Test Value')
-        widont(six.u('Test Value'))
+        self.assertEqual(widont('Test Value'), 'Test&nbsp;Value')
+        self.assertEqual(widont(six.u('Test Value')), u'Test&nbsp;Value')
 
     def test_widont_html(self):
-        widont_html('Test Value')
-        widont_html(six.u('Test Value'))
+        self.assertEqual(widont_html('Test Value'), 'Test&nbsp;Value')
+        self.assertEqual(widont_html(six.u('Test Value')), u'Test&nbsp;Value')
