20a94a310ae9eb40400566c8e7d89a8a99010af8
trbs <trbs@trbs.net>
2016-07-02 21:24:37 -0400

    fix create_command and update sample command

--- django_extensions/management/commands/create_command.py
+++ django_extensions/management/commands/create_command.py
@@ -3,6 +3,7 @@ import os
 import sys
 import shutil
 
+from django.apps import apps
 from django.core.management.base import AppCommand
 from django.core.management.color import color_style
 
@@ -12,7 +13,6 @@ from django_extensions.management.utils import _make_writeable, signalcommand
 class Command(AppCommand):
     help = ("Creates a Django management command directory structure for the given app name"
             " in the app's directory.")
-    args = "[appname]"
     label = 'application name'
 
     requires_system_checks = False
@@ -21,24 +21,16 @@ class Command(AppCommand):
     can_import_settings = True
 
     def add_arguments(self, parser):
+        parser.add_argument('app_name')
         parser.add_argument(
             '--name', '-n', action='store', dest='command_name',
             default='sample',
             help='The name to use for the management command')
-        parser.add_argument(
-            '--base', '-b', action='store', dest='base_command',
-            default='Base', help='The base class used for implementation of '
-            'this command. Should be one of Base, App, Label, or NoArgs')
 
     @signalcommand
-    def handle_app_config(self, app, **options):
+    def handle(self, *args, **options):
+        app = apps.get_app_config(options['app_name'])
         copy_template('command_template', app.path, **options)
-
-    @signalcommand
-    def handle_app(self, app, **options):
-        # handle_app is RemovedInDjango19
-        app_dir = os.path.dirname(app.__file__)
-        copy_template('command_template', app_dir, **options)
 
 
 def copy_template(template_name, copy_to, **options):
@@ -53,14 +45,6 @@ def copy_template(template_name, copy_to, **options):
 
     template_dir = os.path.join(django_extensions.__path__[0], 'conf', template_name)
 
-    handle_method = "handle(self, *args, **options)"
-    if base_command == 'AppCommand':
-        handle_method = "handle_app(self, app, **options)"
-    elif base_command == 'LabelCommand':
-        handle_method = "handle_label(self, label, **options)"
-    elif base_command == 'NoArgsCommand':
-        handle_method = "handle_noargs(self, **options)"
-
     # walks the template structure and copies it
     for d, subdirs, files in os.walk(template_dir):
         relative_dir = d[len(template_dir) + 1:]
@@ -70,7 +54,7 @@ def copy_template(template_name, copy_to, **options):
             if subdir.startswith('.'):
                 del subdirs[i]
         for f in files:
-            if f.endswith('.pyc') or f.startswith('.DS_Store'):
+            if f.endswith('.pyc') or f.endswith('.pyo') or f.startswith('.DS_Store') or f.startswith('__pycache__'):
                 continue
             path_old = os.path.join(d, f)
             path_new = os.path.join(copy_to, relative_dir, f.replace('sample', command_name)).rstrip(".tmpl")
@@ -84,7 +68,7 @@ def copy_template(template_name, copy_to, **options):
                 print(SUCCESS("%s" % path_new))
             with open(path_old, 'r') as fp_orig:
                 with open(path_new, 'w') as fp_new:
-                    fp_new.write(fp_orig.read().replace('{{ command_name }}', command_name).replace('{{ base_command }}', base_command).replace('{{ handle_method }}', handle_method))
+                    fp_new.write(fp_orig.read().replace('{{ command_name }}', command_name).replace('{{ base_command }}', base_command))
             try:
                 shutil.copymode(path_old, path_new)
                 _make_writeable(path_new)
