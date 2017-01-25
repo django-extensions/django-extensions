3097dfdd4d6e525962982cbdb55907ecdd8c9399
trbs <trbs@trbs.net>
2017-01-25 19:03:11 +0100

    add manage.py for debugging and development

---
+++ manage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.testapp.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
