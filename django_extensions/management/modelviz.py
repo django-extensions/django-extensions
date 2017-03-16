0948f62a541657de455e646b8663a3991b59a66c
Dan Lipsitt <danlipsitt@gmail.com>
2017-03-15 20:23:33 -0700

    modelviz: Handle verbose_names containing unicode.

--- django_extensions/management/modelviz.py
+++ django_extensions/management/modelviz.py
@@ -8,6 +8,8 @@ Based on:
   Adapted to be used with django-extensions
 """
 
+# from __future__ import unicode_literals
+
 import datetime
 import os
 import re
@@ -19,7 +21,7 @@ from django.db.models.fields.related import (
 )
 from django.contrib.contenttypes.fields import GenericRelation
 from django.template import Context, Template, loader
-from django.utils.encoding import force_bytes, force_str
+from django.utils.encoding import force_text
 from django.utils.safestring import mark_safe
 from django.utils.translation import activate as activate_language
 
@@ -41,6 +43,7 @@ __contributors__ = [
     "Adam Dobrawy <naczelnik@jawnosc.tk>",
     "Mikkel Munch Mortensen <https://www.detfalskested.dk/>",
     "Andrzej Bistram <andrzej.bistram@gmail.com>",
+    "Daniel Lipsitt <danlipsitt@gmail.com>",
 ]
 
 
@@ -109,7 +112,7 @@ class ModelGraph(object):
 
     def add_attributes(self, field, abstract_fields):
         if self.verbose_names and field.verbose_name:
-            label = force_bytes(field.verbose_name)
+            label = force_text(field.verbose_name)
             if label.islower():
                 label = label.capitalize()
         else:
@@ -132,7 +135,7 @@ class ModelGraph(object):
 
     def add_relation(self, field, model, extras=""):
         if self.verbose_names and field.verbose_name:
-            label = force_bytes(field.verbose_name)
+            label = force_text(field.verbose_name)
             if label.islower():
                 label = label.capitalize()
         else:
@@ -143,7 +146,7 @@ class ModelGraph(object):
             related_query_name = field.related_query_name()
             if self.verbose_names and related_query_name.islower():
                 related_query_name = related_query_name.replace('_', ' ').capitalize()
-            label = '{} ({})'.format(label, force_str(related_query_name))
+            label = '{} ({})'.format(label, force_text(related_query_name))
 
         # handle self-relationships and lazy-relationships
         if isinstance(field.rel.to, six.string_types):
@@ -207,7 +210,7 @@ class ModelGraph(object):
         }
 
         if self.verbose_names and appmodel._meta.verbose_name:
-            context['label'] = force_bytes(appmodel._meta.verbose_name)
+            context['label'] = force_text(appmodel._meta.verbose_name)
         else:
             context['label'] = context['name']
 
