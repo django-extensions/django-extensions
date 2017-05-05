aa3f6a0eba3553dfdafbbd9ec6e1dc287f7da99f
trbs <trbs@trbs.net>
2017-05-05 16:06:40 +0200

    backport get_random_secret_key only for django versions that dont support it

--- django_extensions/management/commands/generate_secret_key.py
+++ django_extensions/management/commands/generate_secret_key.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand
-from django.utils.crypto import get_random_string
-
 from django_extensions.management.utils import signalcommand
+
+try:
+    from django.core.management.utils import get_random_secret_key
+except ImportError:
+    from django.utils.crypto import get_random_string
+
+    def get_random_secret_key():
+        """
+        Return a 50 character random string usable as a SECRET_KEY setting value.
+        """
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+        return get_random_string(50, chars)
 
 
 class Command(BaseCommand):
@@ -12,5 +22,4 @@ class Command(BaseCommand):
 
     @signalcommand
     def handle(self, *args, **options):
-        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-        return get_random_string(50, chars)
+        return get_random_secret_key()
