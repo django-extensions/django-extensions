68d4b3c5fbf25443724a7d9760dfc53be332251f
Nicolás Mendoza <niccolasmendoza@gmail.com>
2017-06-23 22:22:13 -0300

    WIP. Adding some tests, export_emails command - code refactor, PY2/PY3 compatible

--- django_extensions/management/utils.py
+++ django_extensions/management/utils.py
@@ -2,6 +2,10 @@
 import logging
 import os
 import sys
+import csv
+import codecs
+
+from io import BytesIO
 
 from django_extensions.management.signals import post_command, pre_command
 
@@ -68,3 +72,31 @@ def has_ipdb():
         return True
     except ImportError:
         return False
+
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        self.queue = BytesIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
--- tests/management/commands/test_export_emails.py
+++ tests/management/commands/test_export_emails.py
@@ -1,5 +1,5 @@
-# coding=utf-8
-"""Module testing for manage.py export_emails command"""
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
 from django.core.management import call_command
 
@@ -10,31 +10,66 @@ import pytest
 from tests.testapp.settings import DATABASES
 
 
+@pytest.fixture(autouse=True)
+def custom_djsettings(settings):  # noqa
+    """Custom django settings to avoid warnings in stdout"""
+    settings.TEMPLATE_DEBUG = False
+    settings.DEBUG = False
+    return settings
+
+
 @pytest.fixture(scope='module')
-def django_db_setup():
+def django_db_setup():  # noqa
     """Select default database for testing"""
-    settings.DATABASES = DATABASES
+    settings.DATABASES = DATABASES  # noqa
 
 
-@pytest.fixture(scope='module')
-def django_db_setup(django_db_setup, django_db_blocker):
-    """Load to database a set of users, create for export emails command testing"""
-    with django_db_blocker.unblock():
-        call_command('loaddata', 'group.json')  # Loading initial data,testing, Group: Attendees.
-        call_command('loaddata', 'user.json')   # Loading 20 users, initial data, testing.
+@pytest.fixture(scope='module')  # noqa
+def django_db_setup(django_db_setup, django_db_blocker):  # noqa
+    """Load to database a set of users, create for export
+    emails command testing"""
+    with django_db_blocker.unblock():  # noqa
+        call_command('loaddata', 'group.json')
+        call_command('loaddata', 'user.json')
 
 
-@pytest.mark.django_db
-def test_export_emails_noargs(capsys):
-    """Test: python manage.py export_emails , without args.
-        simple single entry per line in the format of:
-            "full name" <my@address.com>;
-    """
+@pytest.mark.django_db()
+def test_do_export_emails_stdout_start(capsys):
+    """Testing export_emails command without args.stdout starts."""
     export_emails = Command()
     export_emails.run_from_argv(
         ['manage.py', 'export_emails']
     )
 
     out, err = capsys.readouterr()
+    assert out.startswith('"Mijaíl Bulgakóv')
 
 
+@pytest.mark.django_db()
+def test_do_export_emails_stdout_end(capsys):
+    """Testing export_emails command without args.stdout end."""
+    export_emails = Command()
+    export_emails.run_from_argv(['manage.py', 'export_emails'])
+
+    out, err = capsys.readouterr()
+    assert '"Frédéric Mistral" <frederic_mistral@gmail.com>;\n\n' in out
+
+
+@pytest.mark.django_db()
+def test_do_export_emails_format_email(capsys):
+    """Testing python.manage export_emails -f emails"""
+    export_emails = Command()
+    export_emails.run_from_argv(['manage.py', 'export_emails', '--format=emails'])
+
+    out, err = capsys.readouterr()
+    assert 'frederic_mistral@gmail.com' in out
+
+
+@pytest.mark.django_db()
+def test_do_export_emails_format_google(capsys):
+    """Testing python.manage export_emails -f google"""
+    export_emails = Command()
+    export_emails.run_from_argv(['manage.py', 'export_emails', '--format=google'])
+
+    out, err = capsys.readouterr()
+    assert out.startswith('Name,Email')
