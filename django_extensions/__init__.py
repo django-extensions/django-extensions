8fa6f5b0284928406e8029148b0148a41bede7f6
trbs <trbs@trbs.net>
2017-08-22 20:55:29 +0200

    v1.9.0

--- django_extensions/__init__.py
+++ django_extensions/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-VERSION = (1, 8, 2, 'DEV')
+VERSION = (1, 9, 0)
 
 # Dynamically calculate the version based on VERSION tuple
 if len(VERSION) > 2 and VERSION[2] is not None:
