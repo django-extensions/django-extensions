d69ddc00cf05f60834d09d406a4be8d5e0f76cab
Kevin Ndung'u <kevgathuku@gmail.com>
2017-09-11 11:05:19 +0300

    Add test for import errors on scripts

--- tests/test_runscript.py
+++ tests/test_runscript.py
@@ -29,3 +29,13 @@ class RunScriptTests(TestCase):
     def test_prints_error_on_nonexistent_script(self):
         call_command('runscript', 'non_existent_script', verbosity=2)
         self.assertIn("No (valid) module for script 'non_existent_script' found", sys.stdout.getvalue())
+
+    def test_prints_additional_info_on_nonexistent_script_and_low_verbosity(self):
+        call_command('runscript', 'non_existent_script', verbosity=1)
+        self.assertIn("No (valid) module for script 'non_existent_script' found", sys.stdout.getvalue())
+        self.assertIn("Try running with a higher verbosity level like: -v2 or -v3", sys.stdout.getvalue())
+
+    def test_prints_import_error_on_script_with_invalid_imports(self):
+        call_command('runscript', 'invalid_import_script', verbosity=2)
+        self.assertIn("Cannot import module 'tests.testapp.scripts.invalid_import_script'", sys.stdout.getvalue())
+        self.assertRegexpMatches(sys.stdout.getvalue(), 'No module named (\')?(invalidpackage)\1?')
---
+++ tests/testapp/scripts/invalid_import_script.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import invalidpackage # NOQA
+
+
+def run():
+    pass
