ab04276140c23db7c927f83b180e6559bf15abf5
trbs <trbs@trbs.net>
2017-10-31 14:32:03 +0100

    simplify loading replacements setting

--- django_extensions/settings.py
+++ django_extensions/settings.py
@@ -4,7 +4,4 @@ import os
 from django.conf import settings
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-REPLACEMENTS = {
-}
-add_replacements = getattr(settings, 'EXTENSIONS_REPLACEMENTS', {})
-REPLACEMENTS.update(add_replacements)
+REPLACEMENTS = getattr(settings, 'EXTENSIONS_REPLACEMENTS', {})
