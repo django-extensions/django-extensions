03f9f4a36f8151e6f8453fbc1f296c1d68475bf4
trbs <trbs@trbs.net>
2018-01-21 18:32:05 +0100

    add tests for validate_templates (including failure case for PR #1150)

---
+++ tests/management/commands/test_validate_templates.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import pytest
+from django.core.management import call_command, CommandError
+from django.utils.six import StringIO
+
+
+def test_validate_templates():
+    out = StringIO()
+    try:
+        call_command('validate_templates', verbosity=3, stdout=out, stderr=out)
+    except CommandError:
+        print(out.getvalue())
+        raise
+
+    output = out.getvalue()
+    assert "0 errors found\n" in output
+
+
+def test_validate_templates_with_error(settings):
+    settings.INSTALLED_APPS += ['tests.testapp_with_template_errors']
+
+    out = StringIO()
+    with pytest.raises(CommandError, message="1 errors found"):
+        call_command('validate_templates', verbosity=3, stdout=out, stderr=out)
