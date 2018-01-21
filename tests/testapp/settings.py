0a845639fba8b65039dbaff305dacf97c7aa521a
trbs <trbs@trbs.net>
2018-01-21 17:53:56 +0100

    add django.contrib.staticfiles

--- tests/testapp/settings.py
+++ tests/testapp/settings.py
@@ -8,6 +8,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.admin',
     'django.contrib.sessions',
+    'django.contrib.staticfiles',
     'tests.collisions',
     'tests.testapp',
     'tests.testapp_with_no_models_file',
