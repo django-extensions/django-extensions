deabc15ea7accd96bb47d718602110ca32f32930
Goto Hayato <habita.gh@gmail.com>
2018-03-07 10:26:11 +0900

    Issue #1173: Replace `get_random_string()` with `BaseUserManager.make_random_password()` for future maintenance.

--- django_extensions/management/commands/generate_password.py
+++ django_extensions/management/commands/generate_password.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
+from django.contrib.auth.base_user import BaseUserManager
 from django.core.management.base import BaseCommand
-from django.utils.crypto import get_random_string
 from django_extensions.management.utils import signalcommand
 
 
 class Command(BaseCommand):
-    help = "Generates a new password that can be used for a user password."
+    help = "Generates a new password that can be used for a user password. This uses Django core's default password generator `BaseUserManager.make_random_password()`."
 
     requires_system_checks = False
 
     def add_arguments(self, parser):
-        default_length = 10
         parser.add_argument(
-            '--length', nargs='?', type=int, default=default_length,
-            help='Password length (default: %d).' % (default_length, ))
+            '--length', nargs='?', type=int,
+            help='Password length.')
 
     @signalcommand
     def handle(self, *args, **options):
         length = options['length']
+        manager = BaseUserManager()
 
-        # This does not have "I" or "0" or letters and digits that look similar
-        # to avoid confusion.
-        allowed_chars = ('abcdefghjkmnpqrstuvwxyz'
-                         'ABCDEFGHJKLMNPQRSTUVWXYZ'
-                         '23456789')
-        return get_random_string(length, allowed_chars)
+        if length:
+            return manager.make_random_password(length)
+        else:
+            return manager.make_random_password()
