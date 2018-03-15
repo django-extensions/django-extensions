5b95d2ca41d58ffdfae2f3a5f7b11e603e86f918
trbs <trbs@trbs.net>
2018-03-15 20:04:17 +0100

    bump version number

--- django_extensions/__init__.py
+++ django_extensions/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-VERSION = (2, 0, 6)
+VERSION = (2, 0, 7, 'DEV')
 
 # Dynamically calculate the version based on VERSION tuple
 if len(VERSION) > 2 and VERSION[2] is not None:
