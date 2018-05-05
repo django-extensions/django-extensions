0ccd09d4851ce9ae7a0b29d8888e6ac02cc560ce
Leo Antunes <leo@costela.net>
2018-05-05 14:01:18 +0200

    clear_cache: add --all option

--- django_extensions/management/commands/clear_cache.py
+++ django_extensions/management/commands/clear_cache.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 # Author: AxiaCore S.A.S. http://axiacore.com
+from django.conf import settings
 from django.core.cache import DEFAULT_CACHE_ALIAS, caches
 from django.core.cache.backends.base import InvalidCacheBackendError
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 from django_extensions.management.utils import signalcommand
 
@@ -14,11 +15,18 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--cache', action='append',
                             help='Name of cache to clear')
+        parser.add_argument('--all', '-a', action='store_true', default=False,
+                            dest='all_caches', help='Clear all configured caches')
 
     @signalcommand
-    def handle(self, cache, *args, **kwargs):
-        if not cache:
+    def handle(self, cache, all_caches, *args, **kwargs):
+        if not cache and not all_caches:
             cache = [DEFAULT_CACHE_ALIAS]
+        elif cache and all_caches:
+            raise CommandError('Using both --all and --cache is not supported')
+        elif all_caches:
+            cache = getattr(settings, 'CACHES', {DEFAULT_CACHE_ALIAS: {}}).keys()
+
         for key in cache:
             try:
                 caches[key].clear()
