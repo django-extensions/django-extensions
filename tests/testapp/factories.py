c932586b4bba0fec265957688f2239099533501c
kuter <contact@devktr.pl>
2019-01-18 23:32:24 +0100

    tests for NullFieldListFilter and NotNullFieldListFilter

---
+++ tests/testapp/factories.py
@@ -0,0 +1,14 @@
+import factory
+
+
+from .models import Secret
+
+
+class SecretFactory(factory.DjangoModelFactory):
+    """DjangoModelFactory for object Secret."""
+
+    name = factory.Faker('name')
+    text = factory.Faker('bs')
+
+    class Meta:
+        model = Secret
