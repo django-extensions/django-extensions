96e6848a334e7538129759635d5c8b8e600cc712
kuter <contact@devktr.pl>
2019-04-03 23:21:16 +0200

    skip encrypted fields tests if pypy implementation

--- tests/db/fields/test_encrypted.py
+++ tests/db/fields/test_encrypted.py
@@ -17,6 +17,12 @@ try:
 except ImportError:
     from mock import patch
 
+try:
+    from keyczar import keyczar, keyczart, keyinfo  # noqa
+    keyczar_active = True
+except ImportError:
+    keyczar_active = False
+
 
 KEYCZAR_META = '''{"name": "Test", "purpose": "DECRYPT_AND_ENCRYPT", "type": "RSA_PRIV", "encrypted": false, "versions": [{"versionNumber": 1, "status": "PRIMARY", "exportable": false}]}'''  # noqa
 KEYCZAR_1 = '''{"publicKey": {"modulus": "AKljEv64fu7jXoyURILoKcYueSXCodwXuvSgjGAV9I9SWUGAKc_0pj2s5xr8eiuhUrcscdSIFD58JwfdWHikI5YhhkxURBa_1DQ3nkMKWVugzuFk5i_8bVOp0Vp-GOAhHEFfyqxhM1yMJrvarCB23scErLVdM99v7ULrL0_Gcwk2jfoFw8pV7shlJ16WL4mM1yFBrE8Eru-iKkhXoo0XiNgk9PMrmys4q_vnWbcw19V0ywY-ks9Q7d0HzNOYmaWLoTUf4RRR72eeVIcrSAgERLsYW6ukk5sr6QGNWLSgft_nahKQazWlqTP4yRV8iTv-GxAn6IJeeiDzFGJV7myxxXs", "publicExponent": "AQAB", "size": 2048}, "privateExponent": "a7VFGdWlWTAZKhkJq5gHIC7i-CDYHYS1a-I_AMZVOWFqwhzF-aceom80kVNkOCQf3bwUMcnJ3aXCh9y26hetTUTKCfU3SqP2QrHvH8AP7BTIGEi5ml_QAPgl2H4AQVysg-FulchFCZ9Q7fjxpL8Rj8moLLc0Ser5GqlD711IPt150IdhfRiK35_ksskBsH-a-695kg-DO8Gf5T1KX0b79y5HumbViwtJJzrxPvGx9_FyhkqKyaIA9h33J1YBHz4VCD20oEGzBC4WnK-gqLselY3Qcdz3_449vI9-3Y7r69VXFopREm6Noz6s53-Cg0kMs62mCiRJYeJGFtiaqaaEsQ", "primeP": "ANfWqgciiwTLyYAEaoYI6E7R82HoWPooEf1GhqKyqhVtd47Ap-FOKi-APAYF7b4NcbV6SJPl6BqjsiDqsdJ3PnjDY_eUbEEkDjmGuYPKT8pRi8XHEOzxm4ARzMIgysTWXtzteyFFeKmhc3cqM6xORqLei1ZwU5mFM4xCrrbcD6fz", "primeQ": "AMjnuGL1JVvKy5rBOOjoe7ia02vtNzUOsdLSUr5NoFODxsIi64UFhzVmA6xvlK_zZeFWoe784_JpXJGKuJ3Bs_kJR2jgVc71Q0Nc8fn0LIFvkrQ6tMZ5XTfLEIHCH8psHA2Q_vR7yRAACZXfY5we2HB9979p6sOsapqgNgWTyJZZ", "primeExponentP": "awt8whMgrV1AbyxBhH4wcL7xnRI15sMuwNVUeqZvcjcbP4JPAI_anjpUCoywLzzNszqVejxg061x759WV9Jp-ky1bJmA4wG1yFQH99PDpMyyrIEg5NKi026AhZrr1ZmX7KwfEA47XK3E7UnR3Nfpcmc65cDJxW7pdNuxeOFFJn8", "primeExponentQ": "XrDgtjuHHSmLJ2iU0ynoTk0jAwZuc-J2K8CX4TwjKdm9T3k3-p1tadyoNJjuwrN8vGWhs0ucgH_qcmqosyo-Ek2uS58Yso_k4RYosr_ETklxIuNcmwNOzWI3aIE_jJ_B5R8HG-JQFOt_mRUOFOJw7yxgHeblEM7t--0xKRXLgBk", "crtCoefficient": "AMr3lac4iHbnKSO4fwyY2WPaIhKM8VWZC3DEkA9Y4uYt8Xe4diw6K_n0e0t_u46fgBcTzj0vNUCiC_i1kcAuLKH2oZUOvAUhoYnjss9LHu9TRI28QEHj6OW0Gghjr4IyW5jg7SYbQngJmnA4QgbMibqFvtKdBSTf-5okujE8QVSb", "size": 2048}'''  # noqa
@@ -38,6 +44,7 @@ class BaseEncryptedFieldTestCase(TestCase):
         shutil.rmtree(cls.tmpdir)
 
 
+@pytest.mark.skipif(keyczar_active is False, reason="Encrypted fields needs that keyczar is installed")
 class BaseEncryptionFieldExceptions(TestCase):
     """Tests for BaseEncryptedField exceptions."""
 
@@ -62,6 +69,7 @@ class BaseEncryptionFieldExceptions(TestCase):
             BaseEncryptedField()
 
 
+@pytest.mark.skipif(keyczar_active is False, reason="Encrypted fields needs that keyczar is installed")
 class EncryptedCharFieldTests(BaseEncryptedFieldTestCase):
 
     def test_should_return_formfield_with_TextInput_widget(self):
@@ -115,6 +123,7 @@ class EncryptedCharFieldTests(BaseEncryptedFieldTestCase):
         self.assertEqual(internal_type, 'CharField')
 
 
+@pytest.mark.skipif(keyczar_active is False, reason="Encrypted fields needs that keyczar is installed")
 class EncryptedTextFieldTests(BaseEncryptedFieldTestCase):
 
     def test_should_return_formfield_with_Textarea_widget(self):
