4a3eda5711778eeec6041fdd99ac0f4f5d13a86c
kuter <contact@devktr.pl>
2019-04-04 18:53:32 +0200

    Removed get_random_string in favour of get_random_secret_key

--- django_extensions/management/commands/generate_secret_key.py
+++ django_extensions/management/commands/generate_secret_key.py
@@ -1,16 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand
+from django.core.management.utils import get_random_secret_key
+
 from django_extensions.management.utils import signalcommand
-
-try:
-    from django.core.management.utils import get_random_secret_key
-except ImportError:
-    from django.utils.crypto import get_random_string
-
-    def get_random_secret_key():
-        """ Return a 50 character random string usable as a SECRET_KEY setting value. """
-        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-        return get_random_string(50, chars)
 
 
 class Command(BaseCommand):
