f7fc880026ce58db5d082a7ecffe18a2b6429f81
Kuldeep Kumar <langhanikuldeep@gmail.com>
2019-07-25 16:27:51 +0500

    Added Mysql format_field_names testcase

--- tests/test_sqldiff.py
+++ tests/test_sqldiff.py
@@ -6,7 +6,7 @@ from django.apps import apps
 from django.test import TestCase
 
 # from django.core.management import call_command
-from django_extensions.management.commands.sqldiff import SqliteSQLDiff, Command
+from django_extensions.management.commands.sqldiff import SqliteSQLDiff, Command, MySQLDiff
 
 
 class SqlDiffTests(TestCase):
@@ -35,6 +35,22 @@ class SqlDiffTests(TestCase):
     @pytest.mark.skipif(settings.DATABASES['default']['ENGINE'] != 'django.db.backends.sqlite3', reason="Test can only run on sqlite3")
     def test_sql_diff_with_proxy_models(self):
         self._include_proxy_models_testing(True)
+
+    @pytest.mark.skipif(settings.DATABASES['default']['ENGINE'] != 'django.db.backends.mysql', reason="Test can only run on MySql")
+    def test_format_field_names(self):
+        parser = Command().create_parser("test", "sqldiff")
+        args = ["-a"]
+        options = parser.parse_args(args=args)
+        tmp_out = six.StringIO()
+        tmp_err = six.StringIO()
+        instance = MySQLDiff(
+            apps.get_models(include_auto_created=True),
+            vars(options),
+            stdout=tmp_out,
+            stderr=tmp_err,
+        )
+        expected_field_name = ['name', 'email', 'address']
+        self.assertEquals(instance.format_field_names(['Name', 'EMAIL', 'aDDress']), expected_field_name)
 
     # def test_sql_diff_run(self):
     #     tmp_out = six.StringIO()
