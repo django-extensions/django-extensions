bc0aeb08ac06d1e80970f20d952c8cd900d6609d
Kuldeep Kumar <langhanikuldeep@gmail.com>
2019-07-26 17:08:04 +0500

    Added test for sql_to_dict for test_format_fields

--- tests/test_sqldiff.py
+++ tests/test_sqldiff.py
@@ -6,23 +6,27 @@ from django.apps import apps
 from django.test import TestCase
 
 # from django.core.management import call_command
-from django_extensions.management.commands.sqldiff import SqliteSQLDiff, Command, MySQLDiff
+from django_extensions.management.commands.sqldiff import SqliteSQLDiff, Command, MySQLDiff, PostgresqlSQLDiff
 
 
 class SqlDiffTests(TestCase):
+
+    def setUp(self):
+        self.parser = Command().create_parser("test", "sqldiff")
+        self.args = ["-a"]
+        self.options = self.parser.parse_args(args=self.args)
+        self.tmp_out = six.StringIO()
+        self.tmp_err = six.StringIO()
+
     def _include_proxy_models_testing(self, should_include_proxy_models):  # type: (bool) -> ()
-        parser = Command().create_parser("test", "sqldiff")
-        args = ["-a"]
         if should_include_proxy_models:
-            args.append("--include-proxy-models")
-        options = parser.parse_args(args=args)
-        tmp_out = six.StringIO()
-        tmp_err = six.StringIO()
+            self.args.append("--include-proxy-models")
+        self.options = self.parser.parse_args(args=self.args)
         instance = SqliteSQLDiff(
             apps.get_models(include_auto_created=True),
-            vars(options),
-            stdout=tmp_out,
-            stderr=tmp_err,
+            vars(self.options),
+            stdout=self.tmp_out,
+            stderr=self.tmp_err,
         )
         instance.find_differences()
         checked_models = {"%s.%s" % (app_label, model_name) for app_label, model_name, _ in instance.differences}
@@ -36,21 +40,40 @@ class SqlDiffTests(TestCase):
     def test_sql_diff_with_proxy_models(self):
         self._include_proxy_models_testing(True)
 
-    @pytest.mark.skipif(settings.DATABASES['default']['ENGINE'] != 'django.db.backends.mysql', reason="Test can only run on MySql")
     def test_format_field_names(self):
-        parser = Command().create_parser("test", "sqldiff")
-        args = ["-a"]
-        options = parser.parse_args(args=args)
-        tmp_out = six.StringIO()
-        tmp_err = six.StringIO()
         instance = MySQLDiff(
             apps.get_models(include_auto_created=True),
-            vars(options),
-            stdout=tmp_out,
-            stderr=tmp_err,
+            vars(self.options),
+            stdout=self.tmp_out,
+            stderr=self.tmp_err,
         )
         expected_field_name = ['name', 'email', 'address']
         self.assertEquals(instance.format_field_names(['Name', 'EMAIL', 'aDDress']), expected_field_name)
+
+    def test_sql_to_dict(self):
+        mysql_instance = MySQLDiff(
+            apps.get_models(include_auto_created=True),
+            vars(self.options),
+            stdout=self.tmp_out,
+            stderr=self.tmp_err,
+        )
+        sqlite_instance = SqliteSQLDiff(
+            apps.get_models(include_auto_created=True),
+            vars(self.options),
+            stdout=self.tmp_out,
+            stderr=self.tmp_err,
+        )
+        postgresql_instance = PostgresqlSQLDiff(
+            apps.get_models(include_auto_created=True),
+            vars(self.options),
+            stdout=self.tmp_out,
+            stderr=self.tmp_err,
+        )
+        mysql_dict = mysql_instance.sql_to_dict("""select 1 as "foo", 1 + 1 as "BAR";""", [])
+        sqlite_dict = sqlite_instance.sql_to_dict("""select 1 as "foo", 1 + 1 as "BAR";""", [])
+        postgresql_dict = postgresql_instance.sql_to_dict("""select 1 as "foo", 1 + 1 as "BAR";""", [])
+        self.assertEquals(mysql_dict, sqlite_dict)
+        self.assertEquals(mysql_dict, postgresql_dict)
 
     # def test_sql_diff_run(self):
     #     tmp_out = six.StringIO()
