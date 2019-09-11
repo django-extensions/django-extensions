25dad78e137e3aa9e00fa6ab4dbf8fc4b8d53d62
Karran Besen <karranrb@gmail.com>
2019-09-10 21:55:05 -0300

    Style: remove unused import

--- django_extensions/management/commands/print_user_for_session.py
+++ django_extensions/management/commands/print_user_for_session.py
@@ -4,7 +4,6 @@ from __future__ import print_function, unicode_literals
 import importlib
 
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.contrib.sessions.backends.base import VALID_KEY_CHARS
 from django.core.management.base import BaseCommand, CommandError
 from django.utils.module_loading import import_string
