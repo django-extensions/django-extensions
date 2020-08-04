f473e93a541f6c61ad30e03c9588b0b20203f7e0
Michael Käufl <django-extensions@c.michael-kaeufl.de>
2020-08-04 10:35:01 +0200

    Run tests against Django 3.1

--- setup.py
+++ setup.py
@@ -171,6 +171,7 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
