bdf2e51c26303c203d958a8481fbc628ba214dd8
trbs <trbs@trbs.net>
2020-09-07 00:35:01 +0200

    fix mock after changing imports

--- tests/management/commands/test_runserver_plus.py
+++ tests/management/commands/test_runserver_plus.py
@@ -7,6 +7,6 @@ from unittest import mock
 
 @pytest.mark.django_db
 def test_initialize_runserver_plus():
-    with mock.patch('werkzeug.run_simple') as run_simple:
+    with mock.patch('django_extensions.management.commands.runserver_plus.run_simple') as run_simple:
         call_command('runserver_plus')
         assert run_simple.called, 'werkzeug.run_simple was not called'
