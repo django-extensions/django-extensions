2547aaef1015b1ce8d92ef54b40408cefe8f4fe5
trbs <trbs@trbs.net>
2020-10-27 00:33:10 +0100

    fix mocking shutil.copymode for test

--- tests/management/commands/test_create_template_tags.py
+++ tests/management/commands/test_create_template_tags.py
@@ -7,7 +7,7 @@ from django.test import TestCase
 from io import StringIO
 from tests import testapp_with_no_models_file
 
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 
 TEMPLATETAGS_DIR = os.path.join(testapp_with_no_models_file.__path__[0], 'templatetags')
@@ -32,11 +32,9 @@ class CreateTemplateTagsTests(TestCase):
         self.assertTrue(os.path.isfile(os.path.join(TEMPLATETAGS_DIR, 'custom_name_tags.py')))
 
     @patch('sys.stderr', new_callable=StringIO)
-    def test_should_print_error_notice_on_OSError(self, m_stderr):
-        m_shutil = Mock()
-        m_shutil.copymode.side_effect = OSError
-        with patch.dict('sys.modules', shutil=m_shutil):
-            call_command('create_template_tags', 'testapp_with_no_models_file')
+    @patch('shutil.copymode', side_effect=OSError)
+    def test_should_print_error_notice_on_OSError(self, m_copymode, m_stderr):
+        call_command('create_template_tags', 'testapp_with_no_models_file')
 
         self.assertRegex(
             m_stderr.getvalue(),
