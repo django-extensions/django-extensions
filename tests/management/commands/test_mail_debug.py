9c8601112db023546425e84084781df606bf277c
trbs <trbs@trbs.net>
2021-02-07 21:54:33 +0100

    test with different addrport

--- tests/management/commands/test_mail_debug.py
+++ tests/management/commands/test_mail_debug.py
@@ -5,5 +5,5 @@ from unittest import mock
 
 def test_initialize_mail_server():
     with mock.patch('django_extensions.management.commands.mail_debug.asyncore.loop') as loop:
-        call_command('mail_debug')
+        call_command('mail_debug', '2525')
         assert loop.called, 'asyncore.loop was not called'
