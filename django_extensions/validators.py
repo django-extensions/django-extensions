dec0fdd081ed93f7a4754e20935aa382f65a9536
abhiabhi94 <13880786+abhiabhi94@users.noreply.github.com>
2021-06-15 06:57:27 +0530

    fix: Max length validation on HexValidator
    - also add tests for this validator.
    - fix a bug on the equality function that would be caused due to the absence of a default error message.

--- django_extensions/validators.py
+++ django_extensions/validators.py
@@ -83,6 +83,8 @@ class HexValidator:
         self.max_length = max_length
         if message:
             self.message = message
+        else:
+            self.message = self.messages['invalid']
         if code:
             self.code = code
 
@@ -92,7 +94,7 @@ class HexValidator:
             raise ValidationError(self.messages['length'], code='hex_only_length', params={'length': self.length})
         if self.min_length and len(value) < self.min_length:
             raise ValidationError(self.messages['min_length'], code='hex_only_min_length', params={'min': self.min_length})
-        if self.max_length and len(value) < self.max_length:
+        if self.max_length and len(value) > self.max_length:
             raise ValidationError(self.messages['max_length'], code='hex_only_max_length', params={'max': self.max_length})
 
         try:
