5647714313e555e6615093619e3f81326e5d1d58
Denis Kazakov <kazakovdeyu@yandex.ru>
2021-06-20 14:03:28 +0300

    updated arguments descriptions

--- django_extensions/management/commands/managestate.py
+++ django_extensions/management/commands/managestate.py
@@ -16,18 +16,19 @@ DEFAULT_STATE = 'default'
 
 class Command(BaseCommand):
     help = 'Manage database state in the convenient way.'
-    common_args = common_options = None
+    migrate_args = migrate_options = None
     database = filename = None
 
     def add_arguments(self, parser):
         parser.add_argument(
             'action', choices=('dump', 'load'),
-            help='An action to do. Dump action saves applied migrations to the file. '
-                 'Load action applies migrations loaded from the file.',
+            help='An action to do. '
+                 'Dump action saves applied migrations to a file. '
+                 'Load action applies migrations specified in a file.',
         )
         parser.add_argument(
             '-d', '--database', default=DEFAULT_DB_ALIAS,
-            help='Nominates a database to synchronize. Defaults to the "default" database.',
+            help=f'Nominates a database to synchronize. Defaults to the "{DEFAULT_DB_ALIAS}" database.',
         )
         parser.add_argument(
             '-f', '--filename', default=DEFAULT_FILENAME,
@@ -35,40 +36,46 @@ class Command(BaseCommand):
         )
         parser.add_argument(
             '-s', '--state', default=DEFAULT_STATE,
-            help='A name of a state. May be a name of a git branch.',
+            help=f'A name of a state. Usually a name of a git branch. Defaults to "{DEFAULT_STATE}"',
         )
 
         # migrate command arguments
         parser.add_argument(
             '--noinput', '--no-input', action='store_false', dest='interactive',
-            help='Tells Django to NOT prompt the user for input of any kind.',
+            help='The argument for "migrate" command. '
+                 'Tells Django to NOT prompt the user for input of any kind.',
         )
         parser.add_argument(
             '--fake', action='store_true',
-            help='Mark migrations as run without actually running them.',
+            help='The argument for "migrate" command. '
+                 'Mark migrations as run without actually running them.',
         )
         parser.add_argument(
             '--fake-initial', action='store_true',
-            help='Detect if tables already exist and fake-apply initial migrations if so. Make sure '
+            help='The argument for "migrate" command. '
+                 'Detect if tables already exist and fake-apply initial migrations if so. Make sure '
                  'that the current database schema matches your initial migration before using this '
                  'flag. Django will only check for an existing table name.',
         )
         parser.add_argument(
             '--plan', action='store_true',
-            help='Shows a list of the migration actions that will be performed.',
+            help='The argument for "migrate" command. '
+                 'Shows a list of the migration actions that will be performed.',
         )
         parser.add_argument(
             '--run-syncdb', action='store_true',
-            help='Creates tables for apps without migrations.',
+            help='The argument for "migrate" command. '
+                 'Creates tables for apps without migrations.',
         )
         parser.add_argument(
             '--check', action='store_true', dest='check_unapplied',
-            help='Exits with a non-zero status if unapplied migrations exist.',
+            help='The argument for "migrate" command. '
+                 'Exits with a non-zero status if unapplied migrations exist.',
         )
 
     def handle(self, action, database, filename, state, *args, **options):
-        self.common_args = args
-        self.common_options = options
+        self.migrate_args = args
+        self.migrate_options = options
         self.database = database
         self.filename = filename
         getattr(self, action)(state)
@@ -83,12 +90,12 @@ class Command(BaseCommand):
     def load(self, state: str):
         """Apply migrations from the file"""
         migrations = self.read().get(state)
-        if not migrations:
+        if migrations is None:
             raise CommandError(f'No such state saved: {state}')
 
         for app, migration in migrations.items():
-            args = (app, migration, *self.common_args)
-            kwargs = {**self.common_options, 'database': self.database}
+            args = (app, migration, *self.migrate_args)
+            kwargs = {**self.migrate_options, 'database': self.database}
             call_command('migrate', *args, **kwargs)
 
     def read(self) -> dict:
@@ -100,7 +107,7 @@ class Command(BaseCommand):
         with open(self.filename) as file:
             data = json.load(file)
 
-        data.pop('saved_at')
+        data.pop('updated_at')
         return data
 
     def write(self, data: dict):
@@ -110,6 +117,6 @@ class Command(BaseCommand):
         except CommandError:
             saved = {}
 
-        saved.update(data, saved_at=str(timezone.now()))
+        saved.update(data, updated_at=str(timezone.now()))
         with open(self.filename, 'w') as file:
             json.dump(saved, file, indent=2, sort_keys=True)
