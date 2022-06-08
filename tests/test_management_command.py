896aede78acd89b0b00f82a2170abe97b44f30e0
Steve Kowalik <steven@wedontsleep.org>
2022-06-08 17:25:29 +0200

    Drop use of mock and six
    Now that Python >= 3.5 is the minimal version supported, remove all
    remaining traces of the external mock module, as well as one use of six
    that is no longer required.

--- tests/test_management_command.py
+++ tests/test_management_command.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
-import mock
+from unittest import mock
 import logging
 import importlib
 
--- tests/test_sqldiff.py
+++ tests/test_sqldiff.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import mock
+from unittest import mock
 import pytest
 from io import StringIO
 
