3d2bf316960f1e8e957a8a3fe3a5f1d6232c4589
Anita Hammer <166057949+anitahammer@users.noreply.github.com>
2024-04-08 00:14:07 +0100

    Update shell_plus.py

--- django_extensions/management/commands/shell_plus.py
+++ django_extensions/management/commands/shell_plus.py
@@ -333,7 +333,31 @@ class Command(BaseCommand):
     def get_plain(self, options):
         # Using normal Python shell
         import code
+
+        # Set up a dictionary to serve as the environment for the shell.
         imported_objects = self.get_imported_objects(options)
+
+        use_pythonrc = options['use_pythonrc']
+        no_startup = options['no_startup']
+
+        # We want to honor both $PYTHONSTARTUP and .pythonrc.py, so follow system
+        # conventions and get $PYTHONSTARTUP first then .pythonrc.py.
+        if use_pythonrc or not no_startup:
+            for pythonrc in OrderedSet([os.environ.get("PYTHONSTARTUP"), os.path.expanduser('~/.pythonrc.py')]):
+                if not pythonrc:
+                    continue
+                if not os.path.isfile(pythonrc):
+                    continue
+                with open(pythonrc) as handle:
+                    pythonrc_code = handle.read()
+                # Match the behavior of the cpython shell where an error in
+                # PYTHONSTARTUP prints an exception and continues.
+                try:
+                    exec(compile(pythonrc_code, pythonrc, 'exec'), imported_objects)
+                except Exception:
+                    traceback.print_exc()
+                    if self.tests_mode:
+                        raise
 
         # By default, this will set up readline to do tab completion and to read and
         # write history to the .python_history file, but this can be overridden by
@@ -371,28 +395,6 @@ class Command(BaseCommand):
                 readline.parse_and_bind("bind ^I rl_complete")
             else:
                 readline.parse_and_bind("tab:complete")
-
-        use_pythonrc = options['use_pythonrc']
-        no_startup = options['no_startup']
-
-        # We want to honor both $PYTHONSTARTUP and .pythonrc.py, so follow system
-        # conventions and get $PYTHONSTARTUP first then .pythonrc.py.
-        if use_pythonrc or not no_startup:
-            for pythonrc in OrderedSet([os.environ.get("PYTHONSTARTUP"), os.path.expanduser('~/.pythonrc.py')]):
-                if not pythonrc:
-                    continue
-                if not os.path.isfile(pythonrc):
-                    continue
-                with open(pythonrc) as handle:
-                    pythonrc_code = handle.read()
-                # Match the behavior of the cpython shell where an error in
-                # PYTHONSTARTUP prints an exception and continues.
-                try:
-                    exec(compile(pythonrc_code, pythonrc, 'exec'), imported_objects)
-                except Exception:
-                    traceback.print_exc()
-                    if self.tests_mode:
-                        raise
 
         def run_plain():
             code.interact(local=imported_objects)
