d9f9ea9362b16174fcc7e87fa5bbdff257e57f6f
Zadigo <pendenquejohn@gmail.com>
2025-07-23 14:52:19 +0200

    Document expected boto3 variables

--- django_extensions/management/commands/sync_s3.py
+++ django_extensions/management/commands/sync_s3.py
@@ -18,9 +18,9 @@ Note: This script requires the Python boto library and valid Amazon Web
 Services API keys.
 
 Required settings.py variables:
-AWS_ACCESS_KEY_ID = ''
-AWS_SECRET_ACCESS_KEY = ''
-AWS_BUCKET_NAME = ''
+AWS_S3_ACCESS_KEY_ID = ''
+AWS_S3_SECRET_ACCESS_KEY = ''
+AWS_STORAGE_BUCKET_NAME = ''
 
 When you call this command with the `--renamegzip` param, it will add
 the '.gz' extension to the file name. But Safari just doesn't recognize
